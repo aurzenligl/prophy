6657fa7bd00fa7274139bb45705848d617639b2c
Majchrzy <majchrzy@9DDC4Q1.nsn-intra.net>
2014-02-07 14:08:29 +0100

    One file xml parsing to one py : stage 1

--- jinja/messages.py
+++ jinja/messages.py
@@ -17,42 +17,24 @@ class Parser(object):
     files=[]
 
     def __init__(self,xml_dir_path):
-        self.tree_file
-        self.script_dir=os.path.dirname(os.path.realpath(__file__))+"/"	#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-        self.set_isar_xml_dir_path(xml_dir_path)
+        self.tree_files=[]
+        self.xml_dir = xml_dir_path
+        self.script_dir=os.path.dirname(os.path.realpath(__file__))
         self.set_files_to_parse()
         self.open_files()
-
-    def set_isar_xml_dir_path(self,xml_dir_path):
-        self.xml_dir=xml_dir_path
-
-    def open_file(self,file):
-        DOMtree= minidom.parse(self.xml_dir+file)
-        print dir(DOMtree)
-        return DOMtree
 
     def open_files(self):
         for x in self.files:
             self.tree_files.append(self.open_file(x))
-
-    def set_files_to_parse(self):
-        all_files=os.listdir(self.xml_dir)
-        for f in all_files:
-            if f.endswith('.xml'):
-                self.files.append(f)
-        print self.files
 
     def delete_old_files(self,files):
         for f in files:
             os.remove(f)
 
-    def messages_or_struct_parse(self,tree_node,element_name,out_dir):
-        env = Environment(loader=FileSystemLoader(self.script_dir+'/templates'))	#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-        template = env.get_template('message.txt')
-
     def open_file(self,file):
-        DOMtree= minidom.parse(self.xml_dir+file)
-        return DOMtree
+        file_dir=os.path.join(self.xml_dir,file)
+        dom_tree= minidom.parse(file_dir)
+        return dom_tree
 
     def set_files_to_parse(self):
         all_files=os.listdir(self.xml_dir)
@@ -61,25 +43,19 @@ class Parser(object):
                 self.files.append(f)
         print self.files
 
-    def open_files(self):
-        for x in self.files:
-            self.tree_files.append(self.open_file(x))
-
-    def messages_or_struct_parse(self,element_name,out_dir):
-        env = Environment(loader=FileSystemLoader(self.script_dir+'/templates'))#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-        template = env.get_template('message.txt')
-        if not os.path.exists(self.script_dir+out_dir):
-            os.makedirs(self.script_dir+out_dir)
-        messageNodes =tree_node.getElementsByTagName(element_name)
-        for p in messageNodes:
+    def struct_parse(self,tree_node,element_name):
+        tmp_dict={}
+        struct_dict={}
+        struct_nodes =tree_node.getElementsByTagName(element_name)
+        for p in struct_nodes:
             if p.hasChildNodes():
                 name=p.attributes["name"].value
                 member=p.getElementsByTagName('member')
                 for k in member:
-                    self.tmp_dict=self.checkin_dynamic_fields(k)
-                with open(out_dir+"/"+name+".py", 'w') as f:
-                    f.write(template.render(name=name,elements=self.tmp_dict))
-                self.tmp_dict.clear()
+                    tmp_dict=self.checkin_dynamic_fields(k)
+                struct_dict[name]=tmp_dict.copy()
+                tmp_dict.clear()
+        return struct_dict
 
     def checkin_dynamic_fields(self,k,dyn_dict=OrderedDict()):
         value=k.attributes["type"].value
@@ -102,56 +78,44 @@ class Parser(object):
         return dyn_dict	
 
     def enum_parse(self,tree_node):
-        env = Environment(loader=FileSystemLoader(self.script_dir+'/templates'))
-        template = env.get_template('enum.txt')
-        if not os.path.exists(self.script_dir+"/enum"):
-            os.makedirs(self.script_dir+"/enum")
+        tmp_dict={}
+        enum_dict={}
         enum_nodes=tree_node.getElementsByTagName('enum')
         for enum_element in enum_nodes:
             name=enum_element.attributes["name"].value
             member=enum_element.getElementsByTagName('enum-member')
             for member_enum_element in member:
                 value=member_enum_element.getAttribute('value')
-                self.enum_dict[member_enum_element.attributes["name"].value]=value
-            with open("enum/"+name+".py", 'w') as f:
-     			f.write(template.render(enum_name=name,enum=self.enum_dict))
-     		self.enum_dict.clear()
-        
+                tmp_dict[member_enum_element.attributes["name"].value]=value
+            enum_dict[name]=tmp_dict.copy()
+            tmp_dict.clear()
+        return enum_dict
+
     def typedef_parse(self,tree_node):
-        env = Environment(loader=FileSystemLoader(self.script_dir+'/templates'))#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-        template = env.get_template('typedef.txt')
-        if not os.path.exists(self.script_dir+"/typedef"):#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-            os.makedirs(self.script_dir+"/typedef")#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
+        typedef_dict={}
         typedefNodes=tree_node.getElementsByTagName('typedef')
         for typedef_element in typedefNodes:
             if typedef_element.hasAttribute("type"):
-                self.typedef_dict[typedef_element.attributes["name"].value]=typedef_element.attributes["type"].value
-        with open("typedef/"+"typedef.py", 'w') as f:
-            f.write(template.render(typedef=self.typedef_dict))
+                typedef_dict[typedef_element.attributes["name"].value]=typedef_element.attributes["type"].value
+        return typedef_dict
 
-    def constant_parse(self,tree_node):
-        env = Environment(loader=FileSystemLoader(self.script_dir+'/templates'))
-        template = env.get_template('constant.txt')
-        if not os.path.exists(self.script_dir+"/constant"):#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-            os.makedirs(self.script_dir+"/constant")#FIXME:http://docs.python.org/2/library/os.path.html#os.path.join
-        constantNodes=tree_node.getElementsByTagName('constant')
-        if constant_element.hasAttribute("value"):
-            self.constant_dict[constant_element.attributes["name"].value]=constant_element.attributes["value"].value
-        with open("constant/"+"constant.py", 'w') as f:
-            f.write(template.render(constant=self.constant_dict))
-
-    def create_directory(self):
-        pass
+    def constant_parse(self,tree_node, constant_dict={}):
+        constant_nodes=tree_node.getElementsByTagName('constant')
+        for constant_element in constant_nodes:
+            if constant_element.hasAttribute("value"):
+                constant_dict[constant_element.attributes["name"].value]=constant_element.attributes["value"].value
+        return constant_dict
 
     def parsing_xml_files(self):
         for tree_node in self.tree_files:
-            self.create_directory(self)
             self.constant_parse(tree_node)
             self.typedef_parse(tree_node)
             self.enum_parse(tree_node)
-            self.messages_or_struct_parse(tree_node,"message",'msg')
+            self.struct_parse(tree_node,"message")
+            self.struct_parse(tree_node,"struct")
 
 if __name__ == "__main__":
-    xml_path = options.getOptions()
+    options,args = options.getOptions()
+    xml_path=options.isar_path
     parser=Parser(xml_path)
-    #parser.parsing_xml_files()
+    parser.parsing_xml_files()
