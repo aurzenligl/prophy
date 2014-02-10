41de320ba41da726b9c8ceee9943471a6bb977e3
Majchrzy <majchrzy@9DDC4Q1.nsn-intra.net>
2014-02-10 11:00:32 +0100

    const test

--- jinja/test_messages.py
+++ jinja/test_messages.py
@@ -1,5 +1,5 @@
 import messages
-
+import xml.dom
 
 def test_create_of_parser():
     parser = messages.Parser(".")
@@ -8,3 +8,12 @@ def test_create_of_parser():
 def test_of_opening_files():
     parser = messages.Parser(".")
     parser.open_files()
+
+def test_of_constant_sorting():
+    l = ["CONST_A = 5", "CONST_B = CONST_A + 15"]
+
+    example = """<constant comment="Number of different measurement groups in MAC" name="MAX_MEAS_GROUP_TYPE_ID_MAC" value="MAX_MEAS_GROUP_TYPE_ID"/>
+    <constant forceGeneration="true" name="MAX_MEAS_GROUP_TYPE_ID" value="22"/>"""
+
+    tree = xml.dom.minidom.parseString(example)
+    assert l = messages.__constant_sorter(tree)
