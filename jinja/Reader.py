a500badfa8abea407ec326604058ffdfbafbff1d
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-10 10:14:55 +0100

    remove my reader

--- jinja/Reader.py
+++
@@ -1,7 +0,0 @@
-
-
-class Reader(object):
-    @staticmethod
-    def readFile(file_name):
-        f = open(file_name, "r")
-        return f.read()
--- jinja/test_Reader.py
+++
@@ -1,7 +0,0 @@
-import Reader
-
-def test_read_a_file(setup_file_to_read):
-    pass
-    str_a = "W4VNrqXPfDt6eTMLitoWlX7i"
-    file_entry = Reader.Reader.readFile(setup_file_to_read)
-    assert str_a == file_entry
