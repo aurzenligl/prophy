f0d1716811f8ca7375f18195da0be76f250d4dad
Majchrzy <majchrzy@9DDC4Q1.nsn-intra.net>
2014-02-10 17:46:09 +0100

    dummy testy działają

--- jinja/test_messages.py
+++ jinja/test_messages.py
@@ -1,19 +1,12 @@
 import messages
+from reader import XmlReader
 import xml.dom
 
 def test_create_of_parser():
-    parser = messages.Parser(".")
+    parser = messages.Parser()
 
 
 def test_of_opening_files():
-    parser = messages.Parser(".")
-    parser.open_files()
+    reader =XmlReader(".")
+    reader.read_files()
 
-def test_of_constant_sorting():
-    l = ["CONST_A = 5", "CONST_B = CONST_A + 15"]
-
-    example = """<constant comment="Number of different measurement groups in MAC" name="MAX_MEAS_GROUP_TYPE_ID_MAC" value="MAX_MEAS_GROUP_TYPE_ID"/>
-    <constant forceGeneration="true" name="MAX_MEAS_GROUP_TYPE_ID" value="22"/>"""
-    print tree
-    # tree = xml.dom.minidom.parseString(example)
-    # assert l = messages.__constant_sorter(tree)
