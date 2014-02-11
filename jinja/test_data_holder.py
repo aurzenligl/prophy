41b765cbf7a29cfea2964575696fa393b51a3452
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-11 09:46:06 +0100

    fix tests

--- jinja/test_data_holder.py
+++ jinja/test_data_holder.py
@@ -1,57 +1,42 @@
 import os
 import pytest
-
-def sort_list(dic):
-    out_list = []
-
-    for key, val in dic.iteritems():
-        if "_" not in val:
-            out_list.insert(0,key)
-        else:
-            if val in out_list:
-                index = out_list.index(val)
-                out_list.insert(index + 1, key)
-            else:
-                out_list.append(key)
-    return out_list
-
-
+from data_holder import DataHolder as dh
 
 def test_of_sorter():
     dic = {"CONST_A": "CONST_B", "CONST_B": "5", "CONST_C": "4"}
     l = ['CONST_B', 'CONST_A', 'CONST_C']
-    assert l == sort_list(dic)
+    assert l == dh().sort_list(dic)
 
 def test_of_sorter_2():
     dic = {"C_A" : "C_B", "C_B" : "C_C", "C_C" : "5"}
     l = ['C_C', 'C_B', 'C_A']
 
-    assert l == sort_list(dic)
+    assert l == dh().sort_list(dic)
 
 def test_of_sorter_3():
     dic = {"C_A1" : "C_B", "C_A2" : "C_B" , "C_B" : "5"}
     l = ['C_B', 'C_A2', 'C_A1']
-    assert l == sort_list(dic)
+    assert l == dh().sort_list(dic)
 
 def test_of_sorter_4():
     dic = {"C_C" : "5", "C_B":"C_C", "C_A1":"C_B", "C_A2":"C_B"}
     l = ['C_C', 'C_B', 'C_A2', 'C_A1']
-    assert l == sort_list(dic)
+    assert l == dh().sort_list(dic)
 
 def test_of_sorter_5():
     dic = {  "C_C" : "C_A + C_B" , "C_A":"5", "C_B":"3"}
     l = ['C_B', 'C_A', 'C_C']
-    assert l == sort_list(dic)
+    assert l == dh().sort_list(dic)
 
 def test_of_sorter_6():
-    dic = {  "C_C" : "C_A + C_B" , "C_A":"5", "C_B":"3"}
+    dic = {"C_C" : "C_A + C_B" , "C_A":"5", "C_B":"3"}
     l = ['C_B', 'C_A', 'C_C']
-    assert l == sort_list(dic)
+    assert l == dh().sort_list(dic)
 
 def test_of_ecev_of_file():
     from dic import dic
 
-    sort = sort_list(dic)
+    sort = dh().sort_list(dic)
 
     f = "";
 
