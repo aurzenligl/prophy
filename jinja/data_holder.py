b0acabb4cbed3998e64f76ad8f7dbf53ddb91a6a
Majchrzy <majchrzy@9DDC4Q1.nsn-intra.net>
2014-02-12 10:02:35 +0100

    AbstractDataHolder

--- jinja/data_holder.py
+++ jinja/data_holder.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 
 class DataHolder(object):
     msg_dict={}
@@ -5,7 +6,6 @@ class DataHolder(object):
     constant_dict={}
     enum_dict={}
     struct_dict={}
-    include_dict={}
 
     def sort_list(self, dic):
         out_list = []
@@ -20,4 +20,55 @@ class DataHolder(object):
                 else:
                     out_list.append(key)
         return out_list
+
+
+class Holder(object):
+
+    def add_to_list(self,element_name,element_value=0):
+        pass
+    def get_list_values(self):
+        pass
+
+class IncludeHolder(Holder):
+
+    include_list = []
+
+    def add_to_list(self,element):
+        self.include_list.append(element)
+
+    def get_list_values(self):
+        return self.include_list
+
+class EnumHolder(Holder):
+
+    name=''
+    enum=namedtuple('enum','enum_name enum_value')
+    enum_list = []
+
+    def __init__(self,name):
+        self_name=name
+
+    def add_to_list(self,element_name,element_value = 0):
+        self.enum_list.append(self.enum(element_name,element_value))
+
+    def get_list_values(self):
+        return self.enum_list
+
+    def get_enum_holder_name(self):
+        return self.name
+
+class ConstantHolder(Holder):
+
+    constant=namedtuple('constant','constant_name constant_value')
+    constant_list = []
+
+    def add_to_list(self,element_name,element_value = 0):
+        self.constant_list.append(self.constant(element_name,element_value))
+
+    def get_list_values(self):
+        return self.enum_list
+
+   
+
+
 
