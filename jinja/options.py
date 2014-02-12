7eef084a47d7b573214b5588743a6907821437ce
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-12 10:28:23 +0100

    ignore of invalid options

--- jinja/options.py
+++ jinja/options.py
@@ -1,8 +1,12 @@
 from optparse import OptionParser
 
 
+class MyOptionParser(OptionParser):
+    def error(self, msg):
+        pass
+
 def getOptions():
-    parser = OptionParser()
+    parser = MyOptionParser()
     parser.add_option("-i", "--isar_path", help="path to isar dir", type="string", action="store", dest="isar_path", default=".")
     parser.add_option("-o", "--output", help="output format", type="string", action="store", dest='output',
     default='python')
