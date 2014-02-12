84b07a3c4c4f9877d0f0634ad49ef677bf89e699
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-12 15:16:34 +0100

    some changes

---
+++ jinja/test_reader.py
@@ -0,0 +1,35 @@
+import reader
+
+import os
+import shutil
+
+def test_of_xml_reader():
+    test_dir_name = "xml_test_files"
+    shutil.rmtree(test_dir_name, ignore_errors=True)
+    os.mkdir(test_dir_name)
+    xml_dir_path =  os.path.join(".", test_dir_name)
+    document = """\
+        <slideshow>
+        <title>Demo slideshow</title>
+        <slide><title>Slide title</title>
+        <point>This is a demo</point>
+        <point>Of a program for processing slides</point>
+        </slide>
+
+        <slide><title>Another demo slide</title>
+        <point>It is important</point>
+        <point>To have more than</point>
+        <point>one slide</point>
+        </slide>
+        </slideshow>
+        """
+    for x in range(0, 10):
+        f = open(os.path.join(xml_dir_path, str(x) + ".xml"),"w");
+        f.write(document)
+        f.close()
+
+    r = reader.XmlReader(xml_dir_path)
+    r.read_files()
+    assert 10 == len(r.return_tree_files())
+
+
--- jinja/test_writer.py
+++ jinja/test_writer.py
@@ -7,17 +7,34 @@ def test_of_WriterFabric_default_writer():
     assert type(writer.WriterFabric.get_writer("test.txt")) is writer.WriterTxt
 
 
-def test_of_Python_Enum_Serializer_enum():
+def test_of_PythonSerializer():
+
+    ih = data_holder.IncludeHolder()
+    for x in range(20, 400, 3):
+        ih.add_to_list("test_include_"+str(x))
+
+
+    enum = data_holder.EnumHolder()
+    for x in range(1, 200):
+        enum.add_to_list("elem_" + str(x), "val_"+ str(x))
+
+    dh = data_holder.DataHolder()
+    dh.enum_dict["test"] = enum
+
+    ps = writer.PythonSerializer()
+    assert "f2ae92ff2ad5e850aaa6077d0d63956b" == hashlib.md5( ps.serialize(dh) ).hexdigest()
+
+def test_of_PythonSerializer_enum():
     ps = writer.PythonSerializer()
     enum = data_holder.EnumHolder()
     for x in range(1, 200):
         enum.add_to_list("elem_" + str(x), "val_"+ str(x))
-    o =  ps._serialize_enum( { "test" : enum } ) 
-    assert "26f524cefc1243e04e18bbee34eac884" ==  hashlib.md5(o).hexdigest()
+    o =  ps._serialize_enum( { "test" : enum } )
+    assert "ca82f0d3706c921db9ac4d37389a9c77" ==  hashlib.md5(o).hexdigest()
 
-def test_of_Python_Enum_Serializer_import():
+def test_of_PythonSerializer_import():
     l = []
-    for x in range(20, 400,3):
+    for x in range(20, 400, 3):
         l.append("test_include_"+str(x))
     ps = writer.PythonSerializer()
     o = ps._serialize_include(l)
