61095a154459e18026cb1edea1978c55afcbf415
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-12 10:59:53 +0100

    add comment

--- jinja/options.py
+++ jinja/options.py
@@ -9,7 +9,7 @@ def getOptions():
     parser = MyOptionParser()
     parser.add_option("-i", "--isar_path", help="path to isar dir", type="string", action="store", dest="isar_path", default=".")
     parser.add_option("-o", "--output", help="output format", type="string", action="store", dest='output',
-    default='python')
+    default='python') #teraz ta opcja bedzie potrzebna w wyborze "grupy" serializotorow do tekstu pythonnowego a nie jak do tej pory do wybrania fabryki
     return parser.parse_args()
 
 if __name__ == "__main__":
