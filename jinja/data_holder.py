6a37332a276e98831bcfba609aa4c40fb37b33ef
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-12 11:12:08 +0100

    fix of bug

--- jinja/data_holder.py
+++ jinja/data_holder.py
@@ -1,25 +1,25 @@
 from collections import namedtuple
 
-class DataHolder(object):
-    msg_dict={}
-    typedef_dict={}
-    constant_dict={}
-    enum_dict={}
-    struct_dict={}
+#class DataHolder(object):
+#    msg_dict={}
+#    typedef_dict={}
+#    constant_dict={}
+#    enum_dict={}
+#    struct_dict={}
 
-    def sort_list(self, dic):
-        out_list = []
+#    def sort_list(self, dic):
+#        out_list = []
 
-        for key, val in dic.iteritems():
-            if "_" not in val:
-                out_list.insert(0,key)
-            else:
-                if val in out_list:
-                    index = out_list.index(val)
-                    out_list.insert(index + 1, key)
-                else:
-                    out_list.append(key)
-        return out_list
+#        for key, val in dic.iteritems():
+#            if "_" not in val:
+#                out_list.insert(0,key)
+#            else:
+#                if val in out_list:
+#                    index = out_list.index(val)
+#                    out_list.insert(index + 1, key)
+#                else:
+#                    out_list.append(key)
+#        return out_list
 
 
 class Holder(object):
@@ -62,19 +62,15 @@ class ConstantHolder(Holder):
     constant=namedtuple('constant','constant_name constant_value')
     constant_list = []
 
-    def add_to_list(self,element_name,element_value = 0):
-        self.constant_list.append(self.constant(element_name,element_value))
+    def add_to_list(self, element_name,element_value = 0):
+        self.constant_list.append(self.constant(element_name, element_value))
 
     def get_list_values(self):
-        return self.enum_list
+        return self.constant_list
 
 
 # my proposal for new Data_Holder class
-# class Data_Holder(object):
-#    def __init__(self, ...):
-#        self.dic = {"include" : include, 
-#            "enum": enum,
-#            "constants" : constant,
-#            ...
-#            }
+class DataHolder(object):
+    def __init__(self, include, enum, constants):
+        self.dic = {"include" : include, "enum": enum, "constants" : constants }
 
