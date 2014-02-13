4494aa4f7f99a3f906803325c542eccb2da94943
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-13 15:47:45 +0100

    coverage of PythonSerializer

--- jinja/test_PythonSerializer.py
+++ jinja/test_PythonSerializer.py
@@ -5,7 +5,7 @@ import data_holder
 import writer
 
 linux_hashes = {
-"test_of_PythonSerializer" : "d77aba9af887b02875be83660dc02ae9",
+"test_of_PythonSerializer" : "0437fc1b78dd0bfd25d9c2ad69e44cb5",
 "test_of_PythonSerializer_enum" : "26f524cefc1243e04e18bbee34eac884",
 "test_of_PythonSerializer_import" : "42b158e97a9e205de2178d6befaeed35"
 }
@@ -23,22 +23,29 @@ def test_of_PythonSerializer():
     ih = data_holder.IncludeHolder()
     th = data_holder.TypeDefHolder()
 
-    for x in range(20, 400, 3):
+    for x in range(20, 400, 60):
         ih.add_to_list("test_include_"+str(x))
         th.add_to_list("td_elem_name_"+str(x), "td_elem_val_"+str(x))
         th.add_to_list("td_elem_name_"+str(x), "i_td_elem_val_"+str(x))
         th.add_to_list("td_elem_name_"+str(x), "u_td_elem_val_"+str(x))
 
     enum = data_holder.EnumHolder()
-    for x in range(1, 200):
+    for x in range(1, 200, 30):
         enum.add_to_list("elem_" + str(x), "val_"+ str(x))
 
-    dh = data_holder.DataHolder( include = ih, typedef = th )
+    const = data_holder.ConstantHolder()
+    const.add_to_list("C_A","5")
+    const.add_to_list("C_B","5")
+    const.add_to_list("C_C", "C_B + C_A")
+
+    dh = data_holder.DataHolder( include = ih, typedef = th , constant = const)
     dh.enum_dict["test"] = enum
 
+    
 
     ps = writer.PythonSerializer()
     o =  ps.serialize(dh)
+    print o
     assert hashes["test_of_PythonSerializer"] == hashlib.md5( o ).hexdigest()
 
 
