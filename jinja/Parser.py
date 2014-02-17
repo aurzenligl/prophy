7c795b8b4b0c97e2f3686dcccc7d347a53ec2daf
Bartosz Skorupa <bartosz.skorupa@nsn.com>
2014-02-17 09:54:41 +0100

    add files

---
+++ jinja/Parser.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+from data_holder import IncludeHolder,TypeDefHolder,ConstantHolder,EnumHolder,MemberHolder,MessageHolder, DataHolder
+
+class Parser(object):
+
+    tmp_dict = OrderedDict()
+    typedef_dict = {}
+    enum_dict = {}
+    constant_dict = {}
+    class_aprot_string="aprot."
+
+    def __struct_parse(self, tree_node, element_name):
+        list = []
+        struct_nodes = tree_node.getElementsByTagName(element_name)
+        for p in struct_nodes:
+            if p.hasChildNodes():
+                msg = MessageHolder()
+                msg.name = p.attributes["name"].value
+                member = p.getElementsByTagName('member')
+                for k in member:
+                    msg.add_to_list(self.__checkin_member_fields(k))
+                list.append(msg)
+        return list
+
+    def __checkin_member_fields(self, k):
+        member = MemberHolder(k.attributes["name"].value,k.attributes["type"].value)
+        if k.hasChildNodes() and k.getElementsByTagName('dimension'):
+            dimension = k.getElementsByTagName('dimension')
+            for item ,dim_val in dimension[0].attributes.items():
+                if 'Comment' not in item:
+                    member.add_to_list(item,dim_val)
+        return member
+
+    def __enum_parse(self, tree_node):
+        dict = {}
+        enum_nodes = tree_node.getElementsByTagName('enum')
+        for enum_element in enum_nodes:
+            if enum_element.hasChildNodes():
+                name = enum_element.attributes["name"].value
+                enum = EnumHolder()
+                member = enum_element.getElementsByTagName('enum-member')
+                for member_enum_element in member:
+                    value = member_enum_element.getAttribute('value')
+                    enum.add_to_list(member_enum_element.attributes["name"].value,value)
+                dict[name] = enum
+        return dict
+
+    def __typedef_parse(self, tree_node):
+        typedef_dict = TypeDefHolder()
+        typedef_nodes = tree_node.getElementsByTagName('typedef')
+        for typedef_element in typedef_nodes:
+            if typedef_element.hasAttribute("type"):
+                typedef_dict.add_to_list(typedef_element.attributes["name"].value,typedef_element.attributes["type"].value)
+            elif typedef_element.hasAttribute("primitiveType"):
+                type = self.__get_type_of_typedef(typedef_element.attributes["primitiveType"].value)
+                typedef_dict.add_to_list(typedef_element.attributes["name"].value,type)
+        return typedef_dict
+
+    def __get_type_of_typedef(self,value):
+        if "8 bit integer unsigned" in value:
+            return "u8"
+        elif "16 bit integer unsigned" in value:
+            return "u16"
+        elif "32 bit integer unsigned" in value:
+            return "u32"
+        elif "64 bit integer unsigned" in value:
+            return "u64"
+        elif "8 bit integer signed" in value:
+            return "i8"
+        elif "16 bit integer signed" in value:
+            return "i16"
+        elif "32 bit integer signed" in value:
+            return "i32"
+        elif "64 bit integer signed" in value:
+            return "i64"
+        elif "32 bit float" in value:
+            # should be r32 ale nie ma teraz w aprocie obsługi
+            return "i32"
+        elif "64 bit float" in value:
+            # should be r64 ale nie ma teraz w aprocie obsługi
+            return "i64"
+
+
+    def __constant_parse(self, tree_node):
+        constant=ConstantHolder()
+        constant_nodes = tree_node.getElementsByTagName('constant')
+        for constant_element in constant_nodes:
+            if constant_element.hasAttribute("value"):
+                constant.add_to_list(constant_element.attributes["name"].value,constant_element.attributes["value"].value)
+        return constant
+
+    def __get_include(self, tree_node):
+            include = IncludeHolder()
+            include_nodes = tree_node.getElementsByTagName("xi:include")
+            for include_element in include_nodes:
+                if include_element.hasAttribute("href"):
+                    x = include_element.attributes["href"].value
+                    x = x.partition('.')[0]
+                    include.add_to_list(x)
+            return include
+
+    def parsing_xml_files(self, tree_node):
+        data_holder = DataHolder()
+        data_holder.constant = self.__constant_parse(tree_node)
+        data_holder.typedef = self.__typedef_parse(tree_node)
+        data_holder.enum_dict = self.__enum_parse(tree_node)
+        data_holder.msgs_list = self.__struct_parse(tree_node, "message")
+        data_holder.struct_list = self.__struct_parse(tree_node, "struct")
+        data_holder.include = self.__get_include(tree_node)
+        return data_holder
+
---
+++ jinja/Serializers.py
@@ -0,0 +1,105 @@
+class PythonSerializer(object):
+    def __init__(self):
+        self.lib_imp="aprot."
+
+    def serialize(self, dataHolder):
+        out = ""
+        out += self._serialize_include(dataHolder.include.get_list()) + os.linesep
+        out += self._serialize_constant(dataHolder.constant) + os.linesep
+        out += self._serialize_typedef(dataHolder.typedef.get_list()) + os.linesep
+        out += self._serialize_enum(dataHolder.enum_dict) + os.linesep
+        out += self._serialize_msgs(dataHolder.struct_list)
+        out += self._serialize_msgs(dataHolder.msgs_list)
+        return out
+
+    def _serialize_enum(self, enum_dic):
+        template = TemplateFabric().get_template("enum.txt");
+        out = ""
+        for key, val in enum_dic.iteritems():
+            out += template.render(key = key, value = val.list)
+            out += os.linesep
+        return out
+
+    def _serialize_typedef(self, typedef_list):
+        out = ""
+        for key, val in typedef_list:
+            if val.startswith('u') or val.startswith('i') or val.startswith('r'):
+                out += key + " = " + self.lib_imp + val + '\n'
+            else:
+                out += key + " = "  + val + '\n'
+        return out
+
+    def _serialize_include(self, include_list):
+        out = "import aprot \n"
+        for inc in include_list:
+            out += "from " + inc + " import *" + '\n'
+        return out
+
+    def _serialize_constant(self, constant):
+        out = ""
+        for key,val in constant.get_sorted_list():
+            out += key + " = " + val + '\n'
+        return out
+
+    def _serialize_msgs(self,msgs_list):
+        out = ""
+
+        def serialize_members(keys):
+            lib_imp = self.lib_imp
+            desc = []
+            for member in keys:
+                if member.type.startswith('u') or member.type.startswith('i') or member.type.startswith('r')  :
+                    lib_imp = self.lib_imp
+                else :
+                    lib_imp = ""
+                if len(member.list) > 0:
+                    desc.append(self._serialize_msg_member(member))
+                else:
+                    desc.append("('{0}',{1}{2})" .format(member.name ,lib_imp, member.type))
+            return ", ".join(desc)
+        for key in msgs_list:
+            out += "class {0}({1}struct):" .format(key.name, self.lib_imp) + "\n"
+            out += "    __metaclass__ = aprot.struct_generator" + "\n"
+            out += "    _descriptor = [" + serialize_members(key.get_list()) + "]\n"
+        return out
+
+    def _serialize_msg_member(self, member):
+        def format_simple_list(a, b):
+            return  "('{0}',{1}), " .format(a, b)
+        def format_array(a, b, c, d):
+            return "('{0}',{1}array({2},bound='{3}'))" .format(a, b,c,d)
+
+        str = ""
+        variable_name_index = member.get_dimension_field_index('variableSizeFieldName')
+        variable_type_index = member.get_dimension_field_index('variableSizeFieldType')
+        size_index = member.get_dimension_field_index('size')
+        min_size_index = member.get_dimension_field_index('minSize')
+        is_variable_index = member.get_dimension_field_index('isVariableSize')
+
+        if len(member.list) == 4 and variable_name_index != -1 and variable_type_index != -1 :
+            str += format_simple_list(member.list[variable_name_index].dimension_field_value, member.list[variable_type_index].dimension_field_value)
+#            str += format_array(member.name, self.lib_imp, member.type, member.list[variable_name_index].dimensimn_field_value)
+        if len(member.list) == 5:
+            str += format_simple_list(member.list[variable_name_index].dimension_field_value, member.list[variable_type_index].dimension_field_value)
+            str += format_array(member.name, self.lib_imp, member.type, member.list[variable_name_index].dimension_field_value)
+        if  variable_type_index == -1 and variable_name_index != -1:
+            str += format_simple_list(member.list[variable_name_index].dimension_field_value,'TNumberOfItems')
+            str += format_array(member.name, self.lib_imp, member.type, member.list[variable_name_index].dimension_field_value)
+        if len(member.list) == 3 and variable_name_index == -1 and variable_type_index == -1 and size_index != -1 and min_size_index != -1:
+            str += format_simple_list('tmpName','TNumberOfItems')
+            str += format_array(member.name, self.lib_imp, member.type,'tmpName')
+        if len(member.list) == 2 and size_index != -1 and is_variable_index != -1:
+            str += format_simple_list('tmpName','TNumberOfItems')
+            str += format_array(member.name, self.lib_imp, member.type,'tmpName')
+        if len(member.list) == 1 and size_index != -1:
+            str +=  format_simple_list('tmpName','TNumberOfItems')
+            str += format_array(member.name, self.lib_imp, member.type,'tmpName')
+
+        if variable_name_index == -1 and variable_type_index != -1:
+            str += "('{0}',{1}), " .format('tmpName', member.list[variable_type_index].dimension_field_value)
+            str += "('{0}',{1}array({2},bound='{3}'))" .format(member.name, self.lib_imp, member.type,'tmpName')
+        if variable_name_index == -1 and variable_type_index == -1:
+            str += "\" variable_name_index == -1 and variable_type_index == -1\""
+
+        return str
+
