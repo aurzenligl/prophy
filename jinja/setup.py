ee13d5ca0ba616b7ce182819feb57afe5ab825de
Majchrzy <majchrzy@9DDC4Q1.nsn-intra.net>
2014-02-20 15:45:17 +0100

    4

---
+++ jinja/setup.py
@@ -0,0 +1,9 @@
+
+from distutils.core import setup
+
+setup(name='protophy',
+      version='1.0',
+      description='protophy library',
+      package_dir = {'': 'lib'},
+      packages=['protophy'],
+     )
