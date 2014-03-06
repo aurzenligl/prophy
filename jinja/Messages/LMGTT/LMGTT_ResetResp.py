efdaeb9ba70163f9bbcea7e81a25ec7c552685d4
unknown <bicz@CNU0086283.nsn-intra.net>
2014-03-06 10:56:15 +0100

    7

---
+++ jinja/Messages/LMGTT/LMGTT_ResetResp.py
@@ -0,0 +1,6 @@
+import sackparser.jinja.templates.LMGTT as LMGTT
+
+lmgtt = LMGTT.LMGTT_ResetResp
+lmgtt.status = 0x00000000
+lmgtt.subunit = 0x00000010
+lmgtt.cause = 0x00000000
---
+++ jinja/Messages/LMGTT/LMGTT_UserDeleteResp.py
@@ -0,0 +1,9 @@
+import sackparser.jinja.templates.LMGTT as LMGTT
+
+lmgtt = LMGTT.LMGTT_UserDeleteResp
+lmgtt.status = 0x00000000
+lmgtt.subunit = 0x00000010
+lmgtt.cause = 0x00000000
+
+print lmgtt
+print repr(lmgtt.encode(">"))
