9d8c72ee0cf3f250900fa1dba20031b2aac5eb96
aurzenligl <aurzenligl@aurzenligl>
2014-03-21 15:09:10 +0100

    refactoring

--- prophyc/tests/conftest.py
+++ prophyc/tests/conftest.py
@@ -4,7 +4,7 @@ import random
 import sys
 import os
 
-sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 random.seed(1984)
 
