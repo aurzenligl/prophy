970c2fdb030b64780820d3c28060b9275f7a5936
aurzenligl <aurzenligl@aurzenligl>
2014-03-24 20:21:25 +0100

    prophyc refactored to facilitate multiple parsers and serializers

--- prophyc/prophyc.py
+++ prophyc/prophyc.py
@@ -1,21 +1,26 @@
 #! /usr/bin/env python
 
-import Serializers
 import options
 import Parser
+import Serializers
 import sys
 import os
 
 if __name__ == "__main__":
     opts = options.parse_options()
-    if opts.sack:
+
+    if opts.isar:
+        parser = Parser.XMLParser()
+
+    elif opts.sack:
         sys.exit("Sack header parsing mode not yet implemented")
 
     if opts.python_out:
-        for input_file in opts.input_files:
-            data_holder = Parser.XMLParser().parse_xml_file(input_file)
-            ps = Serializers.PythonSerializer()
-            o = ps.serialize(data_holder)
+        serializer = Serializers.PythonSerializer()
 
-            out_filename = os.path.join(opts.python_out, os.path.splitext(os.path.split(input_file)[1])[0] + ".py")
-            open(out_filename, "w").write(o)
+    for input_file in opts.input_files:
+        data_holder = parser.parse_xml_file(input_file)
+        output = serializer.serialize(data_holder)
+
+        out_filename = os.path.join(opts.python_out, os.path.splitext(os.path.split(input_file)[1])[0] + ".py")
+        open(out_filename, "w").write(output)
