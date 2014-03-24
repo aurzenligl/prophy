03c7694159ef17b89f3d9f59295322932ea08d2b
aurzenligl <aurzenligl@aurzenligl>
2014-03-24 13:24:01 +0100

    cleanup

--- prophyc/tests/test_messages.py
+++ prophyc/tests/test_messages.py
@@ -4,40 +4,40 @@ from prophyc import Serializers
 from reader import XmlReader
 
 def test_create_of_parser():
-    parser = Parser.get_parser()
+    Parser.get_parser()
 
 def test_of_opening_files():
-    reader =XmlReader(".")
+    reader = XmlReader(".")
     reader.read_files()
 
 def test_of_message_simple():
-
     msg_h = data_holder.MessageHolder()
     msg_h.name = "MAC_L2CallConfigResp"
-    msg_h.add_to_list(data_holder.MemberHolder('messageResult','SMessageResult'))
+    msg_h.add_to_list(data_holder.MemberHolder('messageResult', 'SMessageResult'))
+
     ps = Serializers.get_serializer()
-    o = ps._serialize_msgs([msg_h])
+    output = ps._serialize_msgs([msg_h])
 
-
-    out = "class MAC_L2CallConfigResp(prophy.struct):\n    __metaclass__ = prophy.struct_generator\n    _descriptor = [('messageResult',SMessageResult)]\n"
-    assert out == o
+    assert output == ("class MAC_L2CallConfigResp(prophy.struct):\n"
+                      "    __metaclass__ = prophy.struct_generator\n"
+                      "    _descriptor = [('messageResult',SMessageResult)]\n")
 
 def test_of_message_complex():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "MAC_L2CallConfigResp"
-    msg_h.add_to_list(data_holder.MemberHolder('messageResult','SMessageResult'))
-    msg_h.add_to_list(data_holder.MemberHolder('lnCelId','TCellId'))
-    msg_h.add_to_list(data_holder.MemberHolder('crnti','TCrnti'))
-    msg_h.add_to_list(data_holder.MemberHolder('ueId','TUeId'))
-    msg_h.add_to_list(data_holder.MemberHolder('ueGroup','TUeGroup'))
-    msg_h.add_to_list(data_holder.MemberHolder('transactionId','TTransactionID'))
-    msg_h.add_to_list(data_holder.MemberHolder('spsCrnti','TCrnti'))
-    msg_h.add_to_list(data_holder.MemberHolder('macUserAddress','TAaSysComSicad'))
-    msg_h.add_to_list(data_holder.MemberHolder('raPreambleIndex','TRaPreambleIndex'))
-    msg_h.add_to_list(data_holder.MemberHolder('prachMaskIndex','TPrachMaskIndex'))
-    msg_h.add_to_list(data_holder.MemberHolder('sRbList','SSRbList'))
-    msg_h.add_to_list(data_holder.MemberHolder('dRbList','SDRbList'))
+    msg_h.add_to_list(data_holder.MemberHolder('messageResult', 'SMessageResult'))
+    msg_h.add_to_list(data_holder.MemberHolder('lnCelId', 'TCellId'))
+    msg_h.add_to_list(data_holder.MemberHolder('crnti', 'TCrnti'))
+    msg_h.add_to_list(data_holder.MemberHolder('ueId', 'TUeId'))
+    msg_h.add_to_list(data_holder.MemberHolder('ueGroup', 'TUeGroup'))
+    msg_h.add_to_list(data_holder.MemberHolder('transactionId', 'TTransactionID'))
+    msg_h.add_to_list(data_holder.MemberHolder('spsCrnti', 'TCrnti'))
+    msg_h.add_to_list(data_holder.MemberHolder('macUserAddress', 'TAaSysComSicad'))
+    msg_h.add_to_list(data_holder.MemberHolder('raPreambleIndex', 'TRaPreambleIndex'))
+    msg_h.add_to_list(data_holder.MemberHolder('prachMaskIndex', 'TPrachMaskIndex'))
+    msg_h.add_to_list(data_holder.MemberHolder('sRbList', 'SSRbList'))
+    msg_h.add_to_list(data_holder.MemberHolder('dRbList', 'SDRbList'))
 
     ps = Serializers.get_serializer()
     o = ps._serialize_msgs([msg_h])
@@ -48,7 +48,7 @@ def test_of_message_with_four_dim_without_field_type():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "MAC_L2CallConfigResp"
-    member_h = data_holder.MemberHolder('sRbList','SSRbList')
+    member_h = data_holder.MemberHolder('sRbList', 'SSRbList')
     member_h.add_to_list("isVariableSize", "true")
     member_h.add_to_list("minSize", "1")
     member_h.add_to_list("size", "MAX_NUM_SRB_PER_USER")
@@ -65,7 +65,7 @@ def test_of_message_with_five_dim():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "MAC_CcchDataReceiveInd"
-    member_h = data_holder.MemberHolder('msg3Info','SMsg3Info')
+    member_h = data_holder.MemberHolder('msg3Info', 'SMsg3Info')
     member_h.add_to_list("isVariableSize", "true")
     member_h.add_to_list("minSize", "1")
     member_h.add_to_list("size", "MAX_MSG3_PER_TTI")
@@ -83,7 +83,7 @@ def test_of_message_with_four_dim_without_min_size():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "SSiList"
-    member_h = data_holder.MemberHolder('data','u8')
+    member_h = data_holder.MemberHolder('data', 'u8')
     member_h.add_to_list("isVariableSize", "true")
     member_h.add_to_list("size", "MAX_SI_DATA")
     member_h.add_to_list("variableSizeFieldName", "size")
@@ -100,7 +100,7 @@ def test_of_message_with_three_fields_without_variable_name_and_variable_type():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "MAC_CellSetupReq"
-    member_h = data_holder.MemberHolder('rlcDlLcpInfo','SRlcLcpInfo')
+    member_h = data_holder.MemberHolder('rlcDlLcpInfo', 'SRlcLcpInfo')
     member_h.add_to_list("isVariableSize", "true")
     member_h.add_to_list("size", "MAX_SI_DATA")
     member_h.add_to_list("minSize", "1")
@@ -115,7 +115,7 @@ def test_of_message_with_two_fields_size_and_isVariable():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "MAC_MeasurementReportInd"
-    member_h = data_holder.MemberHolder('measurementGroupTypeList','EMeasurementGroupType')
+    member_h = data_holder.MemberHolder('measurementGroupTypeList', 'EMeasurementGroupType')
     member_h.add_to_list("isVariableSize", "true")
     member_h.add_to_list("size", "MAX_MEAS_GROUP_TYPE_ID_MAC")
 
@@ -130,7 +130,7 @@ def test_of_message_with_one_dim_size():
 
     msg_h = data_holder.MessageHolder()
     msg_h.name = "SCqiParams"
-    member_h = data_holder.MemberHolder('iRi','TIRi')
+    member_h = data_holder.MemberHolder('iRi', 'TIRi')
     member_h.add_to_list("size", "MAX_NUM_OF_RI_PMI_INFORMATION")
     msg_h.add_to_list(member_h)
 
