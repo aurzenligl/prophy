2c9023ead50f9f8a29c900e6f7608ff34e3b4364
aurzenligl <aurzenligl@aurzenligl>
2014-03-24 13:46:03 +0100

    style

--- prophyc/Serializers.py
+++ prophyc/Serializers.py
@@ -10,11 +10,11 @@ def get_serializer():
 
 class PythonSerializer(object):
     def __init__(self):
-        self.lib_imp="prophy."
+        self.lib_imp = "prophy."
 
     def serialize(self, dataHolder):
         out = ""
-        
+
         out += self._serialize_include(dataHolder.include.get_list()) + os.linesep
         out += self._serialize_union(dataHolder.union_dict) + os.linesep
         out += self._serialize_constant(dataHolder.constant) + os.linesep
@@ -30,23 +30,23 @@ class PythonSerializer(object):
         def serialize_enum_members(list):
             desc = []
             for member in list:
-                    k,v = member
+                    k, v = member
                     desc.append("('{0}',{1})" .format(k , v))
             return ", ".join(desc)
         out = ""
-        
+
         for key, val in enum_dic.iteritems():
             out += "class {0}({1}enum):" .format(key, self.lib_imp) + "\n"
             out += "    __metaclass__ = {0}enum_generator" .format(self.lib_imp) + "\n"
             out += "    _enumerators  = [" + serialize_enum_members(val.list) + "]\n"
 
         return out
-    
-#######################################################################
-                        
 
 #######################################################################
-    
+
+
+#######################################################################
+
     def _serialize_typedef(self, dataHolder):
         typedef_list = dataHolder.typedef.get_list()
         struct_list = dataHolder.struct_list
@@ -59,35 +59,35 @@ class PythonSerializer(object):
                 out += key + " = " + self.lib_imp + val + '\n'
             elif val.startswith('S'):
                 if val not in list_used_structed:
-                    out += self._get_struct_for_typedef(val,struct_list) + '\n'  
+                    out += self._get_struct_for_typedef(val, struct_list) + '\n'
                 list_used_structed.append(val)
-                out += key + " = "  + val + '\n'
+                out += key + " = " + val + '\n'
             elif val.startswith('E'):
                 if val not in list_used_enums :
-                    out += self._get_enum_for_typedef(val,enum_dict) + '\n'  
+                    out += self._get_enum_for_typedef(val, enum_dict) + '\n'
                 list_used_enums .append(val)
-                out += key + " = "  + val + '\n'
+                out += key + " = " + val + '\n'
             else:
-                out += key + " = "  + val + '\n'
+                out += key + " = " + val + '\n'
         return out
 
-    def _get_struct_for_typedef(self,val,struct_list):
+    def _get_struct_for_typedef(self, val, struct_list):
         out = ""
         for i in xrange(len(struct_list)):
             if struct_list[i].name == val:
                 x = struct_list.pop(i)
                 return self._serialize_msgs([x])
-        print struct_list[i].name,val
+        print struct_list[i].name, val
         return "1"
 
-    def _get_enum_for_typedef(self,val2,enum_dict):
+    def _get_enum_for_typedef(self, val2, enum_dict):
         out = ""
-        for key,val in enum_dict.iteritems():
+        for key, val in enum_dict.iteritems():
             if key == val2:
-                enum_dict.pop(key,val)                
+                enum_dict.pop(key, val)
                 return self._serialize_enum({key:val})
         return "1"
-        
+
     def _serialize_include(self, include_list):
         out = "import {0} \n" .format(self.lib_imp[:-1])
         for inc in include_list:
@@ -96,7 +96,7 @@ class PythonSerializer(object):
 
     def _serialize_constant(self, constant):
         out = ""
-        for key,val in constant.get_sorted_list():
+        for key, val in constant.get_sorted_list():
             out += key + " = " + val + '\n'
         return out
 
@@ -104,11 +104,11 @@ class PythonSerializer(object):
         def serialize_union_members(list):
             desc = []
             for member in list:
-                    k,v = member
+                    k, v = member
                     desc.append("('{0}',{1})" .format(k , v))
             return ", ".join(desc)
         out = ""
-        
+
         for key, val in union_dict.iteritems():
             out += "class {0}({1}union):" .format(key, self.lib_imp) + "\n"
             out += "    __metaclass__ = {0}union_generator" .format(self.lib_imp) + "\n"
@@ -117,7 +117,7 @@ class PythonSerializer(object):
 
         return out
 
-    def _serialize_msgs(self,msgs_list):
+    def _serialize_msgs(self, msgs_list):
         out = ""
 
         def serialize_members(keys):
@@ -130,7 +130,7 @@ class PythonSerializer(object):
                 if len(member.list) > 0:
                     desc.append(self._serialize_msg_member(member))
                 else:
-                    desc.append("('{0}',{1}{2})" .format(member.name ,lib_imp, member.type))
+                    desc.append("('{0}',{1}{2})" .format(member.name , lib_imp, member.type))
             return ", ".join(desc)
         for key in msgs_list:
             out += "class {0}({1}struct):" .format(key.name, self.lib_imp) + "\n"
@@ -142,11 +142,11 @@ class PythonSerializer(object):
         def format_simple_list(a, b):
             return  "('{0}',{1}), " .format(a, b)
         def format_array(a, b, c, d):
-            return "('{0}',{1}array({2},bound='{3}'))" .format(a, b,c,d)
-        def format_bytes_list(a,b,c):
-            return  "('{0}',{1}bytes(size={2}))" .format(a,b,c)
-        def format_variable_bytes_list(a,b,c,d):
-            return  "('{0}',{1}bytes(size={2},bound='{3}'))" .format(a,b,c,d)
+            return "('{0}',{1}array({2},bound='{3}'))" .format(a, b, c, d)
+        def format_bytes_list(a, b, c):
+            return  "('{0}',{1}bytes(size={2}))" .format(a, b, c)
+        def format_variable_bytes_list(a, b, c, d):
+            return  "('{0}',{1}bytes(size={2},bound='{3}'))" .format(a, b, c, d)
 
         str = ""
         variable_name_index = member.get_dimension_field_index('variableSizeFieldName')
@@ -165,10 +165,10 @@ class PythonSerializer(object):
             variable_type = member.list[variable_type_index].dimension_field_value
 
         if len(member.list) == 1 and size_index != -1:
-            str += format_bytes_list(member.name, self.lib_imp,member.list[size_index].dimension_field_value )
+            str += format_bytes_list(member.name, self.lib_imp, member.list[size_index].dimension_field_value)
         else:
-            str += format_simple_list(variable_name,variable_type)
-            str += format_array(member.name, self.lib_imp, member.type,variable_name)
+            str += format_simple_list(variable_name, variable_type)
+            str += format_array(member.name, self.lib_imp, member.type, variable_name)
 
         return str
 
