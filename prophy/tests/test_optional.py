5214471ae8b4fdc9c1e19771be38f7372320ecc0
aurzenligl <aurzenligl@aurzenligl>
2014-04-10 11:56:55 +0200

    tests refactoring

--- prophy/tests/test_optional.py
+++ prophy/tests/test_optional.py
@@ -1,18 +1,21 @@
 import prophy
 import pytest
 
-def test_optional_assignment_scalar():
+def test_optional_scalar():
     class O(prophy.struct):
         __metaclass__ = prophy.struct_generator
         _descriptor = [("a", prophy.optional(prophy.u32))]
 
     x = O()
     assert None == x.a
+    assert "\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
     assert """\
 """ == str(x)
 
     x.a = 10
     assert 10 == x.a
+    assert "\x00\x00\x00\x01\x00\x00\x00\x0a" == x.encode(">")
+    assert "\x01\x00\x00\x00\x0a\x00\x00\x00" == x.encode("<")
     assert """\
 a: 10
 """ == str(x)
@@ -22,7 +25,9 @@ a: 10
     assert """\
 """ == str(x)
 
-def test_optional_assignment_struct():
+    """ add decoding tests """
+
+def test_optional_struct():
     class S(prophy.struct):
         __metaclass__ = prophy.struct_generator
         _descriptor = [("a", prophy.u32)]
@@ -32,14 +37,26 @@ def test_optional_assignment_struct():
 
     x = O()
     assert None == x.a
+    assert "\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
     assert """\
 """ == str(x)
 
     x.a = True
     assert 0 == x.a.a
+    assert "\x00\x00\x00\x01\x00\x00\x00\x00" == x.encode(">")
     assert """\
 a {
   a: 0
+}
+""" == str(x)
+
+    x.a.a = 0xFF
+    assert 0xFF == x.a.a
+    assert "\x00\x00\x00\x01\x00\x00\x00\xFF" == x.encode(">")
+    assert "\x01\x00\x00\x00\xFF\x00\x00\x00" == x.encode("<")
+    assert """\
+a {
+  a: 255
 }
 """ == str(x)
 
@@ -48,72 +65,9 @@ a {
     assert """\
 """ == str(x)
 
-def test_optional_encoding_scalar():
-    class O(prophy.struct):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("a", prophy.optional(prophy.u32))]
-
-    x = O()
-    assert "\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
-
-    x.a = 10
-    assert "\x00\x00\x00\x01\x00\x00\x00\x0a" == x.encode(">")
-    assert "\x01\x00\x00\x00\x0a\x00\x00\x00" == x.encode("<")
-
     """ add decoding tests """
 
-def test_optional_encoding_scalar():
-    class S(prophy.struct):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("a", prophy.u32)]
-    class O(prophy.struct):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("a", prophy.optional(S))]
-
-    x = O()
-    assert "\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
-
-    x.a = True
-    assert "\x00\x00\x00\x01\x00\x00\x00\x00" == x.encode(">")
-
-    x.a.a = 0xFF
-    assert "\x00\x00\x00\x01\x00\x00\x00\xFF" == x.encode(">")
-    assert "\x01\x00\x00\x00\xFF\x00\x00\x00" == x.encode("<")
-
-    """ add decoding tests """
-
-def test_optional_encoding_with_enum():
-    class E(prophy.enum):
-        __metaclass__ = prophy.enum_generator
-        _enumerators = [("E_1", 1),
-                        ("E_2", 2),
-                        ("E_3", 3)]
-    class S(prophy.struct):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("a", E)]
-    class O(prophy.struct):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("a", prophy.optional(E)),
-                       ("b", prophy.optional(S))]
-
-    x = O()
-    assert "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
-
-    x.a = True
-    assert "\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
-
-    x.b = True
-    assert "\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01" == x.encode(">")
-
-    x.b.a = 3
-    assert "\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x03" == x.encode(">")
-
-    x.b = None
-    assert "\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00" == x.encode(">")
-
-    """ add decoding tests """
-
-def test_optional_with_union():
+def test_optional_union():
     class U(prophy.union):
         __metaclass__ = prophy.union_generator
         _descriptor = [("a", prophy.u32, 5)]
@@ -135,8 +89,11 @@ def test_optional_with_union():
 
     """ add decoding tests """
 
-""" prohibit:
-optional of bytes
-optional of array
-union with optional
-"""
+def test_optional_bytes():
+    """ exception """
+
+def test_optional_array():
+    """ exception """
+
+def test_optional_inside_union():
+    """ exception """
