5d501e97f03455134b44664ee3c50ed96acb77d2
aurzenligl <aurzenligl@aurzenligl>
2014-05-28 16:01:13 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name = 'prophy',
         'console_scripts': [
             'prophyc = prophyc.prophyc:main']
         },
-      version = '0.2.3',
+      version = '0.2.4',
       description = 'Binary data interchange format toolchain',
       author = 'Krzysztof Laskowski',
       author_email = 'krzysztof.laskowski@nsn.com',
