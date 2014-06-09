08c8de89b8cde81d33a2787af5e2d062b412991d
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-06-09 17:20:45 +0200

    enum test refactored

--- prophy/tests/test_enum.py
+++ prophy/tests/test_enum.py
@@ -1,139 +1,128 @@
 import prophy
 import pytest
 
-class Enumeration(prophy.enum):
-    __metaclass__ = prophy.enum_generator
-    _enumerators = [("Enumeration_One", 1),
-                    ("Enumeration_Two", 2),
-                    ("Enumeration_Three", 3)]
+@pytest.fixture(scope = 'session')
+def Enumeration():
+    class Enumeration(prophy.enum):
+        __metaclass__ = prophy.enum_generator
+        _enumerators = [("Enumeration_One", 1),
+                        ("Enumeration_Two", 2),
+                        ("Enumeration_Three", 3)]
+    return Enumeration
 
-class Enumeration8(prophy.enum8):
-    __metaclass__ = prophy.enum_generator
-    _enumerators = Enumeration._enumerators
+@pytest.fixture(scope = 'session')
+def Enumeration8():
+    class Enumeration8(prophy.enum8):
+        __metaclass__ = prophy.enum_generator
+        _enumerators = [("Enumeration_One", 1),
+                        ("Enumeration_Two", 2),
+                        ("Enumeration_Three", 3)]
+    return Enumeration8
 
-class TestEnum():
-
+@pytest.fixture(scope = 'session')
+def Enum(Enumeration):
     class Enum(prophy.struct_packed):
         __metaclass__ = prophy.struct_generator
         _descriptor = [("value", Enumeration)]
+    return Enum
 
-    def test_assignment(self):
-        x = self.Enum()
-        assert x.value == 1
-        x.value = "Enumeration_Two"
-        assert x.value == 2
-        x.value = 3
-        assert x.value == 3
-
-        with pytest.raises(Exception):
-            x.value = "Enumeration_Four"
-        with pytest.raises(Exception):
-            x.value = 4
-
-        y = self.Enum()
-        assert y.value == 1
-        y.copy_from(x)
-        assert y.value == 3
-
-    def test_print(self):
-        x = self.Enum()
-        x.value = 2
-        assert str(x) == "value: Enumeration_Two\n"
-
-    def test_encode(self):
-        x = self.Enum()
-        x.value = 2
-        assert x.encode(">") == "\x00\x00\x00\x02"
-
-    def test_decode(self):
-        x = self.Enum()
-        x.decode("\x00\x00\x00\x02", ">")
-        assert x.value == 2
-
-        with pytest.raises(Exception):
-            x.decode("\x00\x00\x00\x09", ">")
-        with pytest.raises(Exception):
-            x.decode("\x00\x00\x01", ">")
-        with pytest.raises(Exception):
-            x.decode("\x00\x00\x00\x00\x01", ">")
-
-    def test_exceptions(self):
-        with pytest.raises(Exception):
-            class NoEnumerators(prophy.enum):
-                __metaclass__ = prophy.enum_generator
-        with pytest.raises(Exception):
-            class NamesOverlapping(prophy.enum):
-                __metaclass__ = prophy.enum_generator
-                _enumerators = [("NamesOverlapping_Overlap", 1),
-                                ("NamesOverlapping_Overlap", 2)]
-        with pytest.raises(Exception):
-            class ValueOutOfBounds(prophy.enum):
-                __metaclass__ = prophy.enum_generator
-                _enumerators = [("OutOfBounds", 0xFFFFFFFF + 1)]
-
-class TestEnum8():
-
-    class Enum(prophy.struct_packed):
+@pytest.fixture(scope = 'session')
+def Enum8(Enumeration8):
+    class Enum8(prophy.struct_packed):
         __metaclass__ = prophy.struct_generator
         _descriptor = [("value", Enumeration8)]
+    return Enum8
 
-    def test_assignment(self):
-        x = self.Enum()
-        assert x.value == 1
-        x.value = "Enumeration_Two"
-        assert x.value == 2
-        x.value = 3
-        assert x.value == 3
+@pytest.fixture(scope = 'session')
+def EnumFixedArray(Enumeration):
+    class EnumFixedArray(prophy.struct_packed):
+         __metaclass__ = prophy.struct_generator
+         _descriptor = [("value", prophy.array(Enumeration, size = 2))]
+    return EnumFixedArray
 
-        with pytest.raises(Exception):
-            x.value = "Enumeration_Four"
-        with pytest.raises(Exception):
-            x.value = 4
+@pytest.fixture(scope = 'session')
+def EnumBoundArray(Enumeration):
+    class EnumBoundArray(prophy.struct_packed):
+        __metaclass__ = prophy.struct_generator
+        _descriptor = [("value_len", prophy.u32),
+                       ("value", prophy.array(Enumeration, bound = "value_len"))]
+    return EnumBoundArray
 
-        y = self.Enum()
-        assert y.value == 1
-        y.copy_from(x)
-        assert y.value == 3
+def test_enum_assignment(Enum):
+    x = Enum()
+    assert x.value == 1
+    x.value = "Enumeration_Two"
+    assert x.value == 2
+    x.value = 3
+    assert x.value == 3
 
-    def test_print(self):
-        x = self.Enum()
-        x.value = 2
-        assert str(x) == "value: Enumeration_Two\n"
+    with pytest.raises(prophy.ProphyError):
+        x.value = "Enumeration_Four"
+    with pytest.raises(prophy.ProphyError):
+        x.value = 4
 
-    def test_encode(self):
-        x = self.Enum()
-        x.value = 2
-        assert x.encode(">") == "\x02"
+    y = Enum()
+    y.copy_from(x)
+    assert y.value == 3
 
-    def test_decode(self):
-        x = self.Enum()
-        x.decode("\x02", ">")
-        assert x.value == 2
+def test_enum_encoding(Enum):
+    x = Enum()
+    x.value = 2
+    assert str(x) == "value: Enumeration_Two\n"
 
-        with pytest.raises(Exception):
-            x.decode("\x09", ">")
-        with pytest.raises(Exception):
-            x.decode("", ">")
-        with pytest.raises(Exception):
-            x.decode("\x00\x01", ">")
+    assert x.encode(">") == "\x00\x00\x00\x02"
 
-    def test_exceptions(self):
-        with pytest.raises(Exception):
-            class NoEnumerators(prophy.enum8):
-                __metaclass__ = prophy.enum_generator
+    x.decode("\x00\x00\x00\x03", ">")
+    assert x.value == 3
 
-        with pytest.raises(Exception) as e:
-            class NamesOverlapping(prophy.enum8):
-                __metaclass__ = prophy.enum_generator
-                _enumerators = [("NamesOverlapping_Overlap", 1),
-                                ("NamesOverlapping_Overlap", 2)]
-        assert "names overlap" == e.value.message
+    with pytest.raises(prophy.ProphyError) as e:
+        x.decode("\x00\x00\x00\x09", ">")
+    assert 'unknown enumerator value' in e.value.message
 
-        with pytest.raises(Exception) as e:
-            class ValueOutOfBounds(prophy.enum8):
-                __metaclass__ = prophy.enum_generator
-                _enumerators = [("OutOfBounds", 256)]
-        assert "out of bounds" == e.value.message
+    with pytest.raises(prophy.ProphyError) as e:
+        x.decode("\x00\x00\x01", ">")
+    assert 'too few bytes to decode integer' in e.value.message
+
+    with pytest.raises(prophy.ProphyError) as e:
+        x.decode("\x00\x00\x00\x01\x01", ">")
+    assert 'not all bytes read' in e.value.message
+
+def test_enum_exceptions():
+    with pytest.raises(Exception):
+        class NoEnumerators(prophy.enum):
+            __metaclass__ = prophy.enum_generator
+    with pytest.raises(Exception):
+        class NamesOverlapping(prophy.enum):
+            __metaclass__ = prophy.enum_generator
+            _enumerators = [("NamesOverlapping_Overlap", 1),
+                            ("NamesOverlapping_Overlap", 2)]
+    with pytest.raises(Exception):
+        class ValueOutOfBounds(prophy.enum):
+            __metaclass__ = prophy.enum_generator
+            _enumerators = [("OutOfBounds", 0xFFFFFFFF + 1)]
+
+def test_enum8_encoding(Enum8):
+    x = Enum8()
+    x.value = 2
+    assert str(x) == "value: Enumeration_Two\n"
+
+    x.value = 2
+    assert x.encode(">") == "\x02"
+
+    x.decode("\x02", ">")
+    assert x.value == 2
+
+    with pytest.raises(prophy.ProphyError) as e:
+        x.decode("\x09", ">")
+    assert 'unknown enumerator value' in e.value.message
+
+    with pytest.raises(prophy.ProphyError) as e:
+        x.decode("", ">")
+    assert 'too few bytes to decode integer' in e.value.message
+
+    with pytest.raises(prophy.ProphyError) as e:
+        x.decode("\x01\x01", ">")
+    assert 'not all bytes read' in e.value.message
 
 def test_enum_with_overlapping_values():
     class ValuesOverlapping(prophy.enum8):
@@ -149,81 +138,61 @@ def test_enum_with_overlapping_values():
     assert "x: ValuesOverlapping_Second\n" == str(x)
 
     x.x = "ValuesOverlapping_Second"
+    assert 42 == x.x
     x.x = "ValuesOverlapping_First"
     assert 42 == x.x
     assert "x: ValuesOverlapping_Second\n" == str(x)
 
-class TestEnumFixedArray():
+def test_enum_fixed_array_assignment(EnumFixedArray):
+    x = EnumFixedArray()
+    assert x.value == [1, 1]
+    assert x.value[0] == 1
+    assert x.value[1] == 1
+    x.value[0] = 2
+    x.value[1] = "Enumeration_Two"
+    assert x.value == [2, 2]
 
-    class Enum(prophy.struct_packed):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("value", prophy.array(Enumeration, size = 2))]
+    y = EnumFixedArray()
+    y.value[:] = [3, 3]
+    y.copy_from(x)
+    assert y.value == [2, 2]
 
-    def test_assignment(self):
-        x = self.Enum()
-        assert x.value[:] == [1, 1]
-        assert x.value[0] == 1
-        assert x.value[1] == 1
-        x.value[0] = 2
-        x.value[1] = "Enumeration_Two"
-        assert x.value[:] == [2, 2]
+def test_enum_fixed_array_encoding(EnumFixedArray):
+    x = EnumFixedArray()
+    x.value[:] = [2, 2]
+    assert str(x) == ("value: Enumeration_Two\n"
+                      "value: Enumeration_Two\n")
 
-        y = self.Enum()
-        assert y.value[:] == [1, 1]
-        y.copy_from(x)
-        assert y.value[:] == [2, 2]
+    x.value[:] = [2, 2]
+    assert x.encode(">") == "\x00\x00\x00\x02\x00\x00\x00\x02"
 
-    def test_print(self):
-        x = self.Enum()
-        x.value[:] = [2, 2]
-        assert str(x) == ("value: Enumeration_Two\n"
-                          "value: Enumeration_Two\n")
+    x.decode("\x00\x00\x00\x02\x00\x00\x00\x02", ">")
+    assert x.value[0] == 2
+    assert x.value[1] == 2
 
-    def test_encode(self):
-        x = self.Enum()
-        x.value[:] = [2, 2]
-        assert x.encode(">") == "\x00\x00\x00\x02\x00\x00\x00\x02"
+def test_enum_bound_array_assignment(EnumBoundArray):
+    x = EnumBoundArray()
+    assert x.value == []
+    x.value[:] = [1, "Enumeration_One"]
+    assert x.value == [1, 1]
+    assert x.value[0] == 1
+    assert x.value[1] == 1
+    x.value[0] = 2
+    x.value[1] = "Enumeration_Two"
+    assert x.value == [2, 2]
 
-    def test_decode(self):
-        x = self.Enum()
-        x.decode("\x00\x00\x00\x02\x00\x00\x00\x02", ">")
-        assert x.value[0] == 2
-        assert x.value[1] == 2
+def test_enum_bound_array_encoding(EnumBoundArray):
+    x = EnumBoundArray()
+    x.value[:] = [2, 2]
+    assert str(x) == ("value: Enumeration_Two\n"
+                      "value: Enumeration_Two\n")
 
-class TestEnumBoundArray():
+    x.value[:] = [2, 2]
+    assert x.encode(">") == "\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02"
 
-    class Enum(prophy.struct_packed):
-        __metaclass__ = prophy.struct_generator
-        _descriptor = [("value_len", prophy.u32),
-                       ("value", prophy.array(Enumeration, bound = "value_len"))]
-
-    def test_assignment(self):
-        x = self.Enum()
-        assert x.value[:] == []
-        x.value[:] = [1, "Enumeration_One"]
-        assert x.value[:] == [1, 1]
-        assert x.value[0] == 1
-        assert x.value[1] == 1
-        x.value[0] = 2
-        x.value[1] = "Enumeration_Two"
-        assert x.value[:] == [2, 2]
-
-    def test_print(self):
-        x = self.Enum()
-        x.value[:] = [2, 2]
-        assert str(x) == ("value: Enumeration_Two\n"
-                          "value: Enumeration_Two\n")
-
-    def test_encode(self):
-        x = self.Enum()
-        x.value[:] = [2, 2]
-        assert x.encode(">") == "\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02"
-
-    def test_decode(self):
-        x = self.Enum()
-        x.decode("\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02", ">")
-        assert x.value[0] == 2
-        assert x.value[1] == 2
+    x.decode("\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02", ">")
+    assert x.value[0] == 2
+    assert x.value[1] == 2
 
 def test_enum_with_0xFFFFFFFF_value():
     class Enum(prophy.enum):
