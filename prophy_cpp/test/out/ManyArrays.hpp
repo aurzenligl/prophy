8d6d8e39fcadf38e1067b8249f118afb0999ea9b
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-06-18 23:59:20 +0200

    struct with many arrays tested

---
+++ prophy_cpp/test/out/ManyArrays.hpp
@@ -0,0 +1,55 @@
+#ifndef _PROPHY_GENERATED_ManyArrays_HPP
+#define _PROPHY_GENERATED_ManyArrays_HPP
+
+#include <prophy/prophy.hpp>
+
+struct ManyArrays
+{
+    uint32_t num_of_x;
+    uint8_t x[1];
+
+    struct part2
+    {
+        uint16_t num_of_y;
+        uint16_t y[1];
+    } _2;
+
+    struct part3
+    {
+        uint8_t num_of_z;
+        uint64_t z[1];
+    } _3;
+};
+
+namespace prophy
+{
+
+inline ManyArrays::part2* swap(ManyArrays::part2& payload)
+{
+    swap(payload.num_of_y);
+    return cast<ManyArrays::part2*>(
+        swap_n_fixed(payload.y, payload.num_of_y));
+}
+
+inline ManyArrays::part3* swap(ManyArrays::part3& payload)
+{
+    swap(payload.num_of_z);
+    return cast<ManyArrays::part3*>(
+        swap_n_fixed(payload.z, payload.num_of_z));
+}
+
+inline ManyArrays* swap(ManyArrays& payload)
+{
+    swap(payload.num_of_x);
+    return cast<ManyArrays*>(
+        swap(*cast<ManyArrays::part3*>(
+            swap(*cast<ManyArrays::part2*>(
+                swap_n_fixed(payload.x, payload.num_of_x)
+            ))
+        ))
+    );
+}
+
+} // namespace prophy
+
+#endif  /* _PROPHY_GENERATED_ManyArrays_HPP */
