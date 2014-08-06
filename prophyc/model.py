78236da558b1f130d7a313258d589b8c3baba05e
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-08-07 00:08:13 +0200

    kinds evaluation added to model

--- prophyc/model.py
+++ prophyc/model.py
@@ -1,5 +1,12 @@
 from collections import namedtuple
 
+class Kind:
+    FIXED = 0
+    DYNAMIC = 1
+    UNLIMITED = 2
+
+""" Model consists of 5 kinds of symbols: Includes, Constants, Enums, Structs, Unions. """
+
 Include = namedtuple("Include", ["name"])
 
 Constant = namedtuple("Constant", ["name", "value"])
@@ -25,6 +32,7 @@ class Struct(object):
     def __init__(self, name, members):
         self.name = name
         self.members = members
+        self.kind = None
 
     def __cmp__(self, other):
         return cmp(other.__dict__, self.__dict__)
@@ -46,6 +54,7 @@ class StructMember(object):
         self.size = size
         self.optional = optional
         self.definition = None
+        self.kind = None
 
     def __cmp__(self, other):
         return cmp(other.__dict__, self.__dict__)
@@ -62,9 +71,18 @@ class StructMember(object):
         fmt = fmts[(self.array, bool(self.bound), bool(self.size), self.optional)]
         return fmt.format(self.type, self.name, self.bound, self.size)
 
+    @property
+    def dynamic(self):
+        return self.bound and not self.size
+
+    @property
+    def greedy(self):
+        return self.array and not self.bound and not self.size
+
 Union = namedtuple("Union", ["name", "members"])
 UnionMember = namedtuple("UnionMember", ["name", "type", "discriminator"])
 
+""" Following functions process model. """
 
 def cross_reference(nodes):
     types = {node.name: node for node in nodes}
@@ -75,3 +93,32 @@ def cross_reference(nodes):
             do_cross_reference(node)
         elif isinstance(node, Struct):
             map(do_cross_reference, node.members)
+
+def evaluate_kinds(nodes):
+    """ Prerequisite to calculate kinds is to cross reference nodes. """
+    def evaluate_node_kind(node):
+        if isinstance(node, Typedef):
+            while isinstance(node, Typedef):
+                node = node.definition
+            return evaluate_node_kind(node)
+        elif isinstance(node, Struct):
+            return node.kind
+        else:
+            return Kind.FIXED
+    def evaluate_member_kind(member):
+        if member.greedy:
+            return Kind.UNLIMITED
+        elif member.dynamic:
+            return Kind.DYNAMIC
+        elif member.definition is None:
+            return Kind.FIXED
+        else:
+            return evaluate_node_kind(member.definition)
+    for node in nodes:
+        if isinstance(node, Struct):
+            for member in node.members:
+                member.kind = evaluate_member_kind(member)
+            if node.members:
+                node.kind = max(x.kind for x in node.members)
+            else:
+                node.kind = Kind.FIXED
--- prophyc/tests/test_model.py
+++ prophyc/tests/test_model.py
@@ -65,7 +65,7 @@ def test_cross_reference_typedef():
             model.StructMember("a", "A"),
             model.StructMember("b", "B")
         ]),
-        model.Typedef("D", "B"),
+        model.Typedef("D", "B")
     ]
 
     model.cross_reference(nodes)
@@ -74,3 +74,127 @@ def test_cross_reference_typedef():
     assert nodes[2].members[1].definition.definition.name == "A"
     assert nodes[3].definition.name == "B"
     assert nodes[3].definition.definition.name == "A"
+
+def test_evaluate_kinds_arrays():
+    nodes = [
+        model.Struct("A", [
+            model.StructMember("a", "u8"),
+            model.StructMember("b", "u8", optional = True),
+            model.StructMember("c", "u8", size = "5"),
+            model.StructMember("d_len", "u8"),
+            model.StructMember("d", "u8", bound = "d_len", size = "5"),
+            model.StructMember("e_len", "u8"),
+            model.StructMember("e", "u8", bound = "e_len"),
+            model.StructMember("f", "u8", unlimited = True)
+        ])
+    ]
+
+    model.cross_reference(nodes)
+    model.evaluate_kinds(nodes)
+
+    assert [x.kind for x in nodes[0].members] == [
+        model.Kind.FIXED,
+        model.Kind.FIXED,
+        model.Kind.FIXED,
+        model.Kind.FIXED,
+        model.Kind.FIXED,
+        model.Kind.FIXED,
+        model.Kind.DYNAMIC,
+        model.Kind.UNLIMITED,
+    ]
+
+def test_evaluate_kinds_struct_records():
+    nodes = [
+        model.Struct("Fix", [
+            model.StructMember("a", "u8")
+        ]),
+        model.Struct("Dyn", [
+            model.StructMember("a_len", "u8"),
+            model.StructMember("a", "u8", bound = "a_len")
+        ]),
+        model.Struct("X", [
+            model.StructMember("a", "Dyn"),
+            model.StructMember("b_len", "u8"),
+            model.StructMember("b", "Fix", bound = "b_len"),
+            model.StructMember("c", "Fix", unlimited = True)
+        ])
+    ]
+
+    model.cross_reference(nodes)
+    model.evaluate_kinds(nodes)
+
+    assert [x.kind for x in nodes] == [
+        model.Kind.FIXED,
+        model.Kind.DYNAMIC,
+        model.Kind.UNLIMITED,
+    ]
+
+    assert [x.kind for x in nodes[2].members] == [
+        model.Kind.DYNAMIC,
+        model.Kind.FIXED,
+        model.Kind.DYNAMIC,
+        model.Kind.UNLIMITED,
+    ]
+
+def test_evaluate_kinds_with_typedefs():
+    nodes = [
+        model.Struct("Empty", []),
+        model.Struct("Dynamic", [
+            model.StructMember("a_len", "u8"),
+            model.StructMember("a", "u8", bound = "a_len")
+        ]),
+        model.Struct("Fixed", [
+            model.StructMember("a", "u8", size = "10")
+        ]),
+        model.Struct("Limited", [
+            model.StructMember("a_len", "u8"),
+            model.StructMember("a", "u8", bound = "a_len", size = "10")
+        ]),
+        model.Struct("Greedy", [
+            model.StructMember("a", "byte", unlimited = True)
+        ]),
+        model.Struct("DynamicWrapper", [
+            model.StructMember("a", "Dynamic")
+        ]),
+        model.Struct("GreedyWrapper", [
+            model.StructMember("a", "Greedy")
+        ]),
+        model.Struct("GreedyDynamic", [
+            model.StructMember("a", "Dynamic", unlimited = True)
+        ]),
+        model.Typedef("TU8", "u8"),
+        model.Typedef("TDynamic", "Dynamic"),
+        model.Typedef("TGreedy", "Greedy"),
+        model.Struct("TypedefedU8", [
+            model.StructMember("a", "TU8")
+        ]),
+        model.Struct("TypedefedDynamic", [
+            model.StructMember("a", "TDynamic")
+        ]),
+        model.Struct("TypedefedGreedy", [
+            model.StructMember("a", "TGreedy")
+        ]),
+        model.Typedef("TTDynamic", "TDynamic"),
+        model.Typedef("TTTDynamic", "TTDynamic"),
+        model.Struct("DeeplyTypedefed", [
+            model.StructMember("a", "TTTDynamic")
+        ]),
+    ]
+
+    model.cross_reference(nodes)
+    model.evaluate_kinds(nodes)
+
+    assert [x.kind for x in nodes if isinstance(x, model.Struct)] == [
+        model.Kind.FIXED,
+        model.Kind.DYNAMIC,
+        model.Kind.FIXED,
+        model.Kind.FIXED,
+        model.Kind.UNLIMITED,
+        model.Kind.DYNAMIC,
+        model.Kind.UNLIMITED,
+        model.Kind.UNLIMITED,
+        model.Kind.FIXED,
+        model.Kind.DYNAMIC,
+        model.Kind.UNLIMITED,
+        model.Kind.DYNAMIC,
+    ]
