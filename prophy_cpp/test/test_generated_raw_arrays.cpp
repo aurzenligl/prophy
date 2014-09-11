a07a2ece4ff33d49c0463ff7db17facd7af9d8ed
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-09-11 23:21:39 +0200

    strings used and compared

--- prophy_cpp/test/test_generated_raw_arrays.cpp
+++ prophy_cpp/test/test_generated_raw_arrays.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include "util.hpp"
 #include "generated_raw/Arrays.ppr.hpp"
 
 using namespace testing;
@@ -9,89 +8,69 @@ using namespace raw;
 
 TEST(generated_raw_arrays, Builtin)
 {
-    data x(
-        "\x01\x00\x00\x02",
+    std::string data("\x01\x00\x00\x02", 4);
+    Builtin* next = prophy::swap(reinterpret_cast<Builtin*>(data.begin().base()));
 
-        "\x01\x00\x02\x00"
-    );
-
-    Builtin* next = prophy::swap(reinterpret_cast<Builtin*>(x.input.data()));
-
-    EXPECT_EQ(byte_distance(x.input.data(), next), 4);
-    EXPECT_THAT(x.input, ContainerEq(x.expected));
+    EXPECT_EQ(4, reinterpret_cast<char*>(next) - data.data());
+    EXPECT_EQ(std::string("\x01\x00\x02\x00", 4), std::string(data, 0, 4));
 }
 
 TEST(generated_raw_arrays, BuiltinFixed)
 {
-    data x(
-        "\x00\x02"
-        "\x00\x02"
-        "\x00\x02",
+    std::string data(
+            "\x00\x02"
+            "\x00\x02"
+            "\x00\x02", 6);
+    BuiltinFixed* next = prophy::swap(reinterpret_cast<BuiltinFixed*>(data.begin().base()));
 
-        "\x02\x00"
-        "\x02\x00"
-        "\x02\x00"
-    );
-
-    BuiltinFixed* next = prophy::swap(reinterpret_cast<BuiltinFixed*>(x.input.data()));
-
-    EXPECT_EQ(byte_distance(x.input.data(), next), 6);
-    EXPECT_THAT(x.input, ContainerEq(x.expected));
+    EXPECT_EQ(6, reinterpret_cast<char*>(next) - data.data());
+    EXPECT_EQ(std::string(
+            "\x02\x00"
+            "\x02\x00"
+            "\x02\x00", 6), std::string(data, 0, 6));
 }
 
 TEST(generated_raw_arrays, BuiltinDynamic)
 {
-    data x(
-        "\x00\x00\x00\x03"
-        "\x00\x05\x00\x06"
-        "\x00\x07\xab\xcd",
+    std::string data(
+            "\x00\x00\x00\x03"
+            "\x00\x05\x00\x06"
+            "\x00\x07\xab\xcd", 12);
+    BuiltinDynamic* next = prophy::swap(reinterpret_cast<BuiltinDynamic*>(data.begin().base()));
 
-        "\x03\x00\x00\x00"
-        "\x05\x00\x06\x00"
-        "\x07\x00\xab\xcd"
-    );
-
-    BuiltinDynamic* next = prophy::swap(reinterpret_cast<BuiltinDynamic*>(x.input.data()));
-
-    EXPECT_EQ(byte_distance(x.input.data(), next), 12);
-    EXPECT_THAT(x.input, ContainerEq(x.expected));
+    EXPECT_EQ(12, reinterpret_cast<char*>(next) - data.data());
+    EXPECT_EQ(std::string(
+            "\x03\x00\x00\x00"
+            "\x05\x00\x06\x00"
+            "\x07\x00\xab\xcd", 12), std::string(data, 0, 12));
 }
 
 TEST(generated_raw_arrays, BuiltinLimited)
 {
-    data x(
-        "\x00\x00\x00\x02"
-        "\x00\x05\x00\x06"
-        "\xab\xcd\xef\xba",
+    std::string data(
+            "\x00\x00\x00\x02"
+            "\x00\x05\x00\x06"
+            "\xab\xcd\xef\xba", 12);
+    BuiltinLimited* next = prophy::swap(reinterpret_cast<BuiltinLimited*>(data.begin().base()));
 
-        "\x02\x00\x00\x00"
-        "\x05\x00\x06\x00"
-        "\xab\xcd\xef\xba"
-    );
-
-    BuiltinLimited* next = prophy::swap(reinterpret_cast<BuiltinLimited*>(x.input.data()));
-
-    EXPECT_EQ(byte_distance(x.input.data(), next), 12);
-    EXPECT_THAT(x.input, ContainerEq(x.expected));
+    EXPECT_EQ(12, reinterpret_cast<char*>(next) - data.data());
+    EXPECT_EQ(std::string(
+            "\x02\x00\x00\x00"
+            "\x05\x00\x06\x00"
+            "\xab\xcd\xef\xba", 12), std::string(data, 0, 12));
 }
 
 TEST(generated_raw_arrays, BuiltinGreedy)
 {
-    data x(
-        "\x00\x08\xab\xcd"
-        "\x00\x00\x00\x01"
-        "\x00\x00\x00\x02",
+    std::string data(
+            "\x00\x08\xab\xcd"
+            "\x00\x00\x00\x01"
+            "\x00\x00\x00\x02", 12);
+    BuiltinGreedy* next = prophy::swap(reinterpret_cast<BuiltinGreedy*>(data.begin().base()));
 
-        "\x08\x00\xab\xcd"
-        "\x01\x00\x00\x00"
-        "\x02\x00\x00\x00"
-    );
-
-    BuiltinGreedy* next = prophy::swap(reinterpret_cast<BuiltinGreedy*>(x.input.data()));
-    uint32_t* past_end = prophy::swap_n_fixed(
-        prophy::cast<uint32_t*>(next), 2);
-
-    EXPECT_EQ(byte_distance(x.input.data(), next), 4);
-    EXPECT_EQ(byte_distance(x.input.data(), past_end), 12);
-    EXPECT_THAT(x.input, ContainerEq(x.expected));
+    EXPECT_EQ(4, reinterpret_cast<char*>(next) - data.data());
+    EXPECT_EQ(std::string(
+            "\x08\x00\xab\xcd"
+            "\x00\x00\x00\x01"
+            "\x00\x00\x00\x02", 12), std::string(data, 0, 12));
 }
