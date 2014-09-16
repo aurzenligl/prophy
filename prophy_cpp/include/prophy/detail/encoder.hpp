4b1ce26fb1cefa4e88f10776d957076d9178f218
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-09-16 23:39:51 +0200

    encoding 16 bit integers also

--- prophy_cpp/include/prophy/detail/encoder.hpp
+++ prophy_cpp/include/prophy/detail/encoder.hpp
@@ -10,6 +10,18 @@ namespace prophy
 namespace detail
 {
 
+inline void encode_little_int(uint8_t* out, const uint16_t& in)
+{
+    out[0] = (in & 0x00FF) >> 0;
+    out[1] = (in & 0xFF00) >> 8;
+}
+
+inline void encode_big_int(uint8_t* out, const uint16_t& in)
+{
+    out[0] = (in & 0xFF00) >> 8;
+    out[1] = (in & 0x00FF) >> 0;
+}
+
 inline void encode_little_int(uint8_t* out, const uint32_t& in)
 {
     out[0] = (in & 0x000000FF) >> 0;
@@ -39,6 +51,24 @@ struct number_encoder<native, T>
 };
 
 template <>
+struct number_encoder<little, int16_t>
+{
+    static void encode(void* data, const uint16_t& x)
+    {
+        encode_little_int(static_cast<uint8_t*>(data), x);
+    }
+};
+
+template <>
+struct number_encoder<little, uint16_t>
+{
+    static void encode(void* data, const int16_t& x)
+    {
+        encode_little_int(static_cast<uint8_t*>(data), static_cast<const uint16_t&>(x));
+    }
+};
+
+template <>
 struct number_encoder<little, int32_t>
 {
     static void encode(void* data, const uint32_t& x)
@@ -52,7 +82,25 @@ struct number_encoder<little, uint32_t>
 {
     static void encode(void* data, const int32_t& x)
     {
-        encode_little_int(static_cast<uint8_t*>(data), x);
+        encode_little_int(static_cast<uint8_t*>(data), static_cast<const uint32_t&>(x));
+    }
+};
+
+template <>
+struct number_encoder<big, int16_t>
+{
+    static void encode(void* data, const int16_t& x)
+    {
+        encode_big_int(static_cast<uint8_t*>(data), static_cast<const uint16_t&>(x));
+    }
+};
+
+template <>
+struct number_encoder<big, uint16_t>
+{
+    static void encode(void* data, const uint16_t& x)
+    {
+        encode_big_int(static_cast<uint8_t*>(data), x);
     }
 };
 
@@ -61,7 +109,7 @@ struct number_encoder<big, int32_t>
 {
     static void encode(void* data, const int32_t& x)
     {
-        encode_big_int(static_cast<uint8_t*>(data), x);
+        encode_big_int(static_cast<uint8_t*>(data), static_cast<const uint32_t&>(x));
     }
 };
 
