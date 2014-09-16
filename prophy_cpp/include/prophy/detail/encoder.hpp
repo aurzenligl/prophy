ebe77895588958b3151827d7435cdd467dfdaf68
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-09-17 00:04:13 +0200

    little refactoring

--- prophy_cpp/include/prophy/detail/encoder.hpp
+++ prophy_cpp/include/prophy/detail/encoder.hpp
@@ -10,19 +10,40 @@ namespace prophy
 namespace detail
 {
 
-inline void encode_little_int(uint8_t* out, const uint16_t& in)
+template <endianness E, typename T>
+inline void encode_int(uint8_t* out, const T& in)
+{
+    *reinterpret_cast<T*>(out) = in;
+}
+
+template <>
+inline void encode_int<little, uint16_t>(uint8_t* out, const uint16_t& in)
 {
     out[0] = (in & 0x00FF) >> 0;
     out[1] = (in & 0xFF00) >> 8;
 }
 
-inline void encode_big_int(uint8_t* out, const uint16_t& in)
+template <>
+inline void encode_int<big, uint16_t>(uint8_t* out, const uint16_t& in)
 {
     out[0] = (in & 0xFF00) >> 8;
     out[1] = (in & 0x00FF) >> 0;
 }
 
-inline void encode_little_int(uint8_t* out, const uint32_t& in)
+template <>
+inline void encode_int<little, int16_t>(uint8_t* out, const int16_t& in)
+{
+    encode_int<little>(out, static_cast<const uint16_t&>(in));
+}
+
+template <>
+inline void encode_int<big, int16_t>(uint8_t* out, const int16_t& in)
+{
+    encode_int<big>(out, static_cast<const uint16_t&>(in));
+}
+
+template <>
+inline void encode_int<little, uint32_t>(uint8_t* out, const uint32_t& in)
 {
     out[0] = (in & 0x000000FF) >> 0;
     out[1] = (in & 0x0000FF00) >> 8;
@@ -30,7 +51,8 @@ inline void encode_little_int(uint8_t* out, const uint32_t& in)
     out[3] = (in & 0xFF000000) >> 24;
 }
 
-inline void encode_big_int(uint8_t* out, const uint32_t& in)
+template <>
+inline void encode_int<big, uint32_t>(uint8_t* out, const uint32_t& in)
 {
     out[0] = (in & 0xFF000000) >> 24;
     out[1] = (in & 0x00FF0000) >> 16;
@@ -38,89 +60,17 @@ inline void encode_big_int(uint8_t* out, const uint32_t& in)
     out[3] = (in & 0x000000FF) >> 0;
 }
 
-template <endianness E, typename T>
-struct number_encoder;
-
-template <typename T>
-struct number_encoder<native, T>
+template <>
+inline void encode_int<little, int32_t>(uint8_t* out, const int32_t& in)
 {
-    static void encode(void* data, const T& x)
-    {
-        *static_cast<T*>(data) = x;
-    }
-};
+    encode_int<little>(out, static_cast<const uint32_t&>(in));
+}
 
 template <>
-struct number_encoder<little, int16_t>
+inline void encode_int<big, int32_t>(uint8_t* out, const int32_t& in)
 {
-    static void encode(void* data, const uint16_t& x)
-    {
-        encode_little_int(static_cast<uint8_t*>(data), x);
-    }
-};
-
-template <>
-struct number_encoder<little, uint16_t>
-{
-    static void encode(void* data, const int16_t& x)
-    {
-        encode_little_int(static_cast<uint8_t*>(data), static_cast<const uint16_t&>(x));
-    }
-};
-
-template <>
-struct number_encoder<little, int32_t>
-{
-    static void encode(void* data, const uint32_t& x)
-    {
-        encode_little_int(static_cast<uint8_t*>(data), x);
-    }
-};
-
-template <>
-struct number_encoder<little, uint32_t>
-{
-    static void encode(void* data, const int32_t& x)
-    {
-        encode_little_int(static_cast<uint8_t*>(data), static_cast<const uint32_t&>(x));
-    }
-};
-
-template <>
-struct number_encoder<big, int16_t>
-{
-    static void encode(void* data, const int16_t& x)
-    {
-        encode_big_int(static_cast<uint8_t*>(data), static_cast<const uint16_t&>(x));
-    }
-};
-
-template <>
-struct number_encoder<big, uint16_t>
-{
-    static void encode(void* data, const uint16_t& x)
-    {
-        encode_big_int(static_cast<uint8_t*>(data), x);
-    }
-};
-
-template <>
-struct number_encoder<big, int32_t>
-{
-    static void encode(void* data, const int32_t& x)
-    {
-        encode_big_int(static_cast<uint8_t*>(data), static_cast<const uint32_t&>(x));
-    }
-};
-
-template <>
-struct number_encoder<big, uint32_t>
-{
-    static void encode(void* data, const uint32_t& x)
-    {
-        encode_big_int(static_cast<uint8_t*>(data), x);
-    }
-};
+    encode_int<big>(out, static_cast<const uint32_t&>(in));
+}
 
 template <endianness E, typename T,
           bool = codec_traits<T>::is_composite,
@@ -132,14 +82,14 @@ struct encoder<E, T, false, false>
 {
     static uint8_t* encode(uint8_t* data, const T& x)
     {
-        number_encoder<E, T>::encode(data, x);
+        encode_int<E>(data, x);
         return data + sizeof(T);
     }
     static uint8_t* encode(uint8_t* data, const T* x, size_t n)
     {
         while (n)
         {
-            number_encoder<E, T>::encode(data, *x);
+            encode_int<E>(data, *x);
             data += sizeof(T);
             ++x;
             --n;
--- prophy_cpp/test/test_encoding_ints.cpp
+++ prophy_cpp/test/test_encoding_ints.cpp
@@ -3,6 +3,8 @@
 #include <prophy/detail/encoder.hpp>
 
 using namespace testing;
+using namespace prophy;
+using namespace prophy::detail;
 
 /// * WARNING * This test group assumes you're running on little endian machine.
 
@@ -10,22 +12,22 @@ TEST(encoding_ints, int16)
 {
     uint16_t data;
 
-    prophy::detail::number_encoder<prophy::native, int16_t>::encode(&data, 0x0102);
+    encode_int<native, int16_t>(reinterpret_cast<uint8_t*>(&data), 0x0102);
     EXPECT_EQ(std::string("\x02\x01", 2), std::string(reinterpret_cast<char*>(&data), 2));
 
-    prophy::detail::number_encoder<prophy::native, uint16_t>::encode(&data, 0x0102);
+    encode_int<native, uint16_t>(reinterpret_cast<uint8_t*>(&data), 0x0102);
     EXPECT_EQ(std::string("\x02\x01", 2), std::string(reinterpret_cast<char*>(&data), 2));
 
-    prophy::detail::number_encoder<prophy::little, int16_t>::encode(&data, 0x0102);
+    encode_int<little, int16_t>(reinterpret_cast<uint8_t*>(&data), 0x0102);
     EXPECT_EQ(std::string("\x02\x01", 2), std::string(reinterpret_cast<char*>(&data), 2));
 
-    prophy::detail::number_encoder<prophy::little, uint16_t>::encode(&data, 0x0102);
+    encode_int<little, uint16_t>(reinterpret_cast<uint8_t*>(&data), 0x0102);
     EXPECT_EQ(std::string("\x02\x01", 2), std::string(reinterpret_cast<char*>(&data), 2));
 
-    prophy::detail::number_encoder<prophy::big, int16_t>::encode(&data, 0x0102);
+    encode_int<big, int16_t>(reinterpret_cast<uint8_t*>(&data), 0x0102);
     EXPECT_EQ(std::string("\x01\x02", 2), std::string(reinterpret_cast<char*>(&data), 2));
 
-    prophy::detail::number_encoder<prophy::big, uint16_t>::encode(&data, 0x0102);
+    encode_int<big, uint16_t>(reinterpret_cast<uint8_t*>(&data), 0x0102);
     EXPECT_EQ(std::string("\x01\x02", 2), std::string(reinterpret_cast<char*>(&data), 2));
 }
 
@@ -33,21 +35,21 @@ TEST(encoding_ints, int32)
 {
     uint32_t data;
 
-    prophy::detail::number_encoder<prophy::native, int32_t>::encode(&data, 0x01020304);
+    encode_int<native, int32_t>(reinterpret_cast<uint8_t*>(&data), 0x01020304);
     EXPECT_EQ(std::string("\x04\x03\x02\x01", 4), std::string(reinterpret_cast<char*>(&data), 4));
 
-    prophy::detail::number_encoder<prophy::native, uint32_t>::encode(&data, 0x01020304);
+    encode_int<native, uint32_t>(reinterpret_cast<uint8_t*>(&data), 0x01020304);
     EXPECT_EQ(std::string("\x04\x03\x02\x01", 4), std::string(reinterpret_cast<char*>(&data), 4));
 
-    prophy::detail::number_encoder<prophy::little, int32_t>::encode(&data, 0x01020304);
+    encode_int<little, int32_t>(reinterpret_cast<uint8_t*>(&data), 0x01020304);
     EXPECT_EQ(std::string("\x04\x03\x02\x01", 4), std::string(reinterpret_cast<char*>(&data), 4));
 
-    prophy::detail::number_encoder<prophy::little, uint32_t>::encode(&data, 0x01020304);
+    encode_int<little, uint32_t>(reinterpret_cast<uint8_t*>(&data), 0x01020304);
     EXPECT_EQ(std::string("\x04\x03\x02\x01", 4), std::string(reinterpret_cast<char*>(&data), 4));
 
-    prophy::detail::number_encoder<prophy::big, int32_t>::encode(&data, 0x01020304);
+    encode_int<big, int32_t>(reinterpret_cast<uint8_t*>(&data), 0x01020304);
     EXPECT_EQ(std::string("\x01\x02\x03\x04", 4), std::string(reinterpret_cast<char*>(&data), 4));
 
-    prophy::detail::number_encoder<prophy::big, uint32_t>::encode(&data, 0x01020304);
+    encode_int<big, uint32_t>(reinterpret_cast<uint8_t*>(&data), 0x01020304);
     EXPECT_EQ(std::string("\x01\x02\x03\x04", 4), std::string(reinterpret_cast<char*>(&data), 4));
 }
