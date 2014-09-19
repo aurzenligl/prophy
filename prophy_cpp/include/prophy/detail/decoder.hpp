94b8d9e709e7b92b95d64953924c52fa5518dc4d
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-09-19 22:34:21 +0200

    decoder sketch

---
+++ prophy_cpp/include/prophy/detail/decoder.hpp
@@ -0,0 +1,48 @@
+#ifndef _PROPHY_DETAIL_DECODER_HPP_
+#define _PROPHY_DETAIL_DECODER_HPP_
+
+#include <stdint.h>
+#include <prophy/endianness.hpp>
+#include <prophy/detail/codec_traits.hpp>
+
+namespace prophy
+{
+namespace detail
+{
+
+template <endianness E, typename T>
+inline void decode_int(T& x, const uint8_t* pos)
+{
+    x = *reinterpret_cast<const T*>(pos);
+}
+
+template <endianness E, typename T,
+          bool = codec_traits<T>::is_composite,
+          bool = codec_traits<T>::size == -1>
+struct decoder;
+
+template <endianness E, typename T>
+struct decoder<E, T, false, false>
+{
+    static bool decode(T& x, const uint8_t*& pos, const uint8_t* end)
+    {
+        if (size_t(end - pos) >= sizeof(T))
+        {
+            decode_int<E>(x, pos);
+            pos += sizeof(T);
+            return true;
+        }
+        return false;
+    }
+};
+
+template <endianness E, typename T>
+inline bool do_decode(T& x, const uint8_t*& pos, const uint8_t* end)
+{
+    return decoder<E, T>::decode(x, pos, end);
+}
+
+} // namespace detail
+} // namespace prophy
+
+#endif /* _PROPHY_DETAIL_DECODER_HPP_ */
--- prophy_cpp/test/test_encoding.cpp
+++ prophy_cpp/test/test_encoding.cpp
@@ -1,10 +1,35 @@
 #include <string>
 #include <gtest/gtest.h>
+#include <prophy/detail/decoder.hpp>
 #include "generated/Dynfields.pp.hpp"
 #include "util.hpp"
 
 using namespace testing;
 using namespace prophy;
+using namespace prophy::detail;
+
+TEST(encoding, decode_builtin_scalars)
+{
+    std::vector<uint8_t> data = bytes("\x01\x00\x00\x00");
+    {
+        const uint8_t* pos = data.data();
+        const uint8_t* end = data.data() + data.size();
+        uint32_t x = 0;
+
+        EXPECT_TRUE(do_decode<native>(x, pos, end));
+        EXPECT_EQ(data.data() + data.size(), pos);
+        EXPECT_EQ(1, x);
+    }
+    {
+        const uint8_t* pos = data.data() + 1;
+        const uint8_t* end = data.data() + data.size();
+        uint32_t x = 0;
+
+        EXPECT_FALSE(do_decode<native>(x, pos, end));
+        EXPECT_EQ(data.data() + 1, pos);
+        EXPECT_EQ(0, x);
+    }
+}
 
 TEST(encoding, endianness)
 {
