472b97413bccaf9266682b58b1ba737e9eee2566
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2014-10-19 21:55:15 +0200

    bug fixed

--- prophyc/tests/generators/test_cpp_full.py
+++ prophyc/tests/generators/test_cpp_full.py
@@ -34,39 +34,47 @@ def Builtin():
 @pytest.fixture
 def Fixcomp():
     return process([
+        model.Struct('Builtin', [
+            model.StructMember('x', 'u32'),
+            model.StructMember('y', 'u32')
+        ]),
         model.Struct('Fixcomp', [
-            model.StructMember('x', 'Fixcomp'),
-            model.StructMember('y', 'Fixcomp')
+            model.StructMember('x', 'Builtin'),
+            model.StructMember('y', 'Builtin')
         ]),
         model.Struct('FixcompFixed', [
-            model.StructMember('x', 'Fixcomp', size = 2)
+            model.StructMember('x', 'Builtin', size = 2)
         ]),
         model.Struct('FixcompDynamic', [
-            model.StructMember('num_of_x', 'Fixcomp'),
-            model.StructMember('x', 'Fixcomp', bound = 'num_of_x')
+            model.StructMember('num_of_x', 'Builtin'),
+            model.StructMember('x', 'Builtin', bound = 'num_of_x')
         ]),
         model.Struct('FixcompLimited', [
-            model.StructMember('num_of_x', 'Fixcomp'),
-            model.StructMember('x', 'Fixcomp', size = 2, bound = 'num_of_x')
+            model.StructMember('num_of_x', 'Builtin'),
+            model.StructMember('x', 'Builtin', size = 2, bound = 'num_of_x')
         ]),
         model.Struct('FixcompGreedy', [
-            model.StructMember('x', 'Fixcomp', unlimited = True)
+            model.StructMember('x', 'Builtin', unlimited = True)
         ])
     ])
 
 @pytest.fixture
 def Dyncomp():
     return process([
+        model.Struct('Builtin', [
+            model.StructMember('x', 'u32'),
+            model.StructMember('y', 'u32')
+        ]),
         model.Struct('Dyncomp', [
-            model.StructMember('x', 'Dyncomp'),
-            model.StructMember('y', 'Dyncomp')
+            model.StructMember('x', 'Builtin'),
+            model.StructMember('y', 'Builtin')
         ]),
         model.Struct('DyncompDynamic', [
-            model.StructMember('num_of_x', 'Dyncomp'),
-            model.StructMember('x', 'Dyncomp', bound = 'num_of_x')
+            model.StructMember('num_of_x', 'Builtin'),
+            model.StructMember('x', 'Builtin', bound = 'num_of_x')
         ]),
         model.Struct('DyncompGreedy', [
-            model.StructMember('x', 'Dyncomp', unlimited = True)
+            model.StructMember('x', 'Builtin', unlimited = True)
         ])
     ])
 
@@ -92,35 +100,35 @@ pos = do_encode<E>(pos, x.x.data(), x.x.size());
 """
 
 def test_generate_fixcomp_encode(Fixcomp):
-    assert generate_struct_encode(Fixcomp[0]) == """\
+    assert generate_struct_encode(Fixcomp[1]) == """\
 pos = do_encode<E>(pos, x.x);
 pos = do_encode<E>(pos, x.y);
 """
-    assert generate_struct_encode(Fixcomp[1]) == """\
+    assert generate_struct_encode(Fixcomp[2]) == """\
 pos = do_encode<E>(pos, x.x, 2);
 """
-    assert generate_struct_encode(Fixcomp[2]) == """\
+    assert generate_struct_encode(Fixcomp[3]) == """\
 pos = do_encode<E>(pos, uint32_t(x.x.size()));
 pos = do_encode<E>(pos, x.x.data(), x.x.size());
 """
-    assert generate_struct_encode(Fixcomp[3]) == """\
+    assert generate_struct_encode(Fixcomp[4]) == """\
 pos = do_encode<E>(pos, uint32_t(std::min(x.x.size(), size_t(2))));
 do_encode<E>(pos, x.x.data(), std::min(x.x.size(), size_t(2)));
 pos = pos + 16;
 """
-    assert generate_struct_encode(Fixcomp[4]) == """\
+    assert generate_struct_encode(Fixcomp[5]) == """\
 pos = do_encode<E>(pos, x.x.data(), x.x.size());
 """
 
 def test_generate_dyncomp_encode(Dyncomp):
-    assert generate_struct_encode(Dyncomp[0]) == """\
+    assert generate_struct_encode(Dyncomp[1]) == """\
 pos = do_encode<E>(pos, x.x);
 pos = do_encode<E>(pos, x.y);
 """
-    assert generate_struct_encode(Dyncomp[1]) == """\
+    assert generate_struct_encode(Dyncomp[2]) == """\
 pos = do_encode<E>(pos, uint32_t(x.x.size()));
 pos = do_encode<E>(pos, x.x.data(), x.x.size());
 """
-    assert generate_struct_encode(Dyncomp[2]) == """\
+    assert generate_struct_encode(Dyncomp[3]) == """\
 pos = do_encode<E>(pos, x.x.data(), x.x.size());
 """
