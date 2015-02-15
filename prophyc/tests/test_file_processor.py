398791583daa3d9defb5e6720ddfff8f0595c1cc
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-02-15 04:09:35 +0100

    process content refactored into plain function

--- prophyc/tests/test_file_processor.py
+++ prophyc/tests/test_file_processor.py
@@ -3,27 +3,26 @@ import re
 
 from prophyc.file_processor import FileProcessor
 
-class InclContProc(object):
-    def __call__(self, content, path, process_file):
-        out = ''
-        for line in content.splitlines():
-            match = re.match('#include <([a-zA-Z0-9_./]*)>', line)
-            if match:
-                include_path = match.groups()[0]
-                out += process_file(include_path)
-            else:
-                out += line + '\n'
-        return out
+def fake_process_content(content, path, process_file):
+    out = ''
+    for line in content.splitlines():
+        match = re.match('#include <([a-zA-Z0-9_./]*)>', line)
+        if match:
+            include_path = match.groups()[0]
+            out += process_file(include_path)
+        else:
+            out += line + '\n'
+    return out
 
 def test_file_processor_single_curdir(tmpdir_cwd):
     open('input.txt', 'w').write('the input\n')
-    assert FileProcessor(InclContProc(), [])('input.txt') == 'the input\n'
+    assert FileProcessor(fake_process_content, [])('input.txt') == 'the input\n'
 
 def test_file_processor_single_nestdir(tmpdir_cwd):
     os.mkdir('x')
     open('x/input.txt', 'w').write('nest input\n')
 
-    assert FileProcessor(InclContProc(), [])('x/input.txt') == 'nest input\n'
+    assert FileProcessor(fake_process_content, [])('x/input.txt') == 'nest input\n'
 
 def test_file_processor_include_dirs(tmpdir_cwd):
     os.mkdir('x')
@@ -34,7 +33,7 @@ def test_file_processor_include_dirs(tmpdir_cwd):
 #include <x.txt>
 #include <y.txt>
 ''')
-    assert FileProcessor(InclContProc(), ['x', 'y'])('input.txt') == '''\
+    assert FileProcessor(fake_process_content, ['x', 'y'])('input.txt') == '''\
 one input
 other input
 '''
