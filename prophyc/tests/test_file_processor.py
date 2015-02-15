69979230148404f34ff737b3cecace0163235e66
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-02-15 03:03:55 +0100

    joining processor changed to including processor

--- prophyc/tests/test_file_processor.py
+++ prophyc/tests/test_file_processor.py
@@ -1,20 +1,27 @@
 import os
-import tempfile
-import pytest
+import re
 
 from prophyc.file_processor import FileProcessor
 
-class JoinContentProcessor(object):
+class InclContProc(object):
     def __call__(self, content, filename, file_processor):
-        return ':'.join((filename, content))
+        out = ''
+        for line in content.splitlines():
+            match = re.match('#include <([a-zA-Z0-9_./]*)>', line)
+            if match:
+                filename = match.groups()[0]
+                out += file_processor(filename)
+            else:
+                out += line + '\n'
+        return out
 
 def test_file_processor_single_curdir(tmpdir_cwd):
-    open('input.txt', 'w').write('the input')
-    proc = FileProcessor(JoinContentProcessor(), [])
-    assert proc('input.txt') == 'input.txt:the input'
+    open('input.txt', 'w').write('the input\n')
+    proc = FileProcessor(InclContProc(), [])
+    assert proc('input.txt') == 'the input\n'
 
 def test_file_processor_single_nestdir(tmpdir_cwd):
     os.mkdir('x')
-    open('x/input.txt', 'w').write('nest input')
-    proc = FileProcessor(JoinContentProcessor(), [])
-    assert proc('x/input.txt') == 'x/input.txt:nest input'
+    open('x/input.txt', 'w').write('nest input\n')
+    proc = FileProcessor(InclContProc(), [])
+    assert proc('x/input.txt') == 'nest input\n'
