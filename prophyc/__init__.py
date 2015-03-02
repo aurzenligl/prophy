3e0aac0ef6bc0440c39a274307adcd9944d635f0
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-03-02 02:13:55 +0100

    only one parse error exception class left

--- prophyc/__init__.py
+++ prophyc/__init__.py
@@ -19,7 +19,7 @@ def main():
     if not opts.input_files:
         emit_error("missing input file")
 
-    parser, parse_error = get_parser(opts)
+    parser = get_parser(opts)
     serializers = get_serializers(opts)
     patcher = get_patcher(opts)
 
@@ -34,7 +34,7 @@ def main():
     for input_file in opts.input_files:
         try:
             nodes = file_parser(input_file)
-        except parse_error as e:
+        except model.ParseError as e:
             sys.exit('\n'.join(('%s: error: %s' % err for err in e.errors)))
 
         for serializer in serializers:
@@ -45,18 +45,17 @@ def main():
                 emit_error(e.message)
 
 def get_parser(opts):
-    '''Returns parser and parse_error class.'''
     if opts.isar:
         from prophyc.parsers.isar import IsarParser
-        return IsarParser(warn = emit_warning), None
+        return IsarParser(warn = emit_warning)
     elif opts.sack:
         if not module_exists("clang"):
             emit_error("sack input requires clang and it's not installed")
         from prophyc.parsers.sack import SackParser
-        return SackParser(opts.include_dirs), None
+        return SackParser(opts.include_dirs)
     else:
-        from prophyc.parsers.prophy import ProphyParser, ParseError
-        return ProphyParser(), ParseError
+        from prophyc.parsers.prophy import ProphyParser
+        return ProphyParser()
 
 def get_serializers(opts):
     serializers = []
