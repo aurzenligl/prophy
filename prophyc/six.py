fa0588e3a87dba0cc51c0eed720288930ac10b57
Mateusz Piorko <mateusz.piorko@nsn.com>
2015-03-18 12:12:04 +0100

    Removed unnecessary code from common library

--- prophyc/six.py
+++ prophyc/six.py
@@ -1,28 +1,9 @@
-# License
-
 import sys
 
 if sys.version < '3':
     from itertools import ifilter
-    long = long
     reduce = reduce
 
-    def b(x):
-        return x
 else:
-    from functools import reduce
-    long = int
     ifilter = filter
-    xrange = range
-
-    def b(x):
-        return x.decode()
-
-def cmp(a, b):
-    return (a > b) - (a < b)
-
-def with_metaclass(meta, *bases):
-    class metaclass(meta):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-    return type.__new__(metaclass, 'temporary_class', (), {})
+    from functools import reduce
