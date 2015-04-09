96af6fa3cd0a345fa14684580d47800d62427cdc
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-04-09 15:38:55 +0200

    bump version to 0.7

--- prophy/tests/test_version.py
+++ prophy/tests/test_version.py
@@ -1,3 +1,3 @@
 def test_version_number():
     import prophy
-    assert prophy.__version__ == '0.5.1dev'
+    assert prophy.__version__ == '0.7'
--- prophyc/__init__.py
+++ prophyc/__init__.py
@@ -7,7 +7,7 @@ from . import options
 from . import model
 from .file_processor import FileProcessor
 
-__version__ = '0.5.1dev'
+__version__ = '0.7'
 
 def main():
     opts = options.parse_options(emit_error)
