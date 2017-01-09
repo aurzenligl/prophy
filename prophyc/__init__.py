6fb9f2ef121b21dceac2d974d783aac49b030244
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-01-10 00:36:16 +0100

    bump version

--- prophyc/__init__.py
+++ prophyc/__init__.py
@@ -7,7 +7,7 @@ from . import options
 from . import model
 from .file_processor import FileProcessor
 
-__version__ = '0.8.0'
+__version__ = '1.0.0'
 
 class Emit(object):
     @staticmethod
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name = 'prophy',
-    version = '0.8.0',
+    version = '1.0.0',
     author = 'Krzysztof Laskowski',
     author_email = 'krzysztof.laskowski@nokia.com',
     maintainer = 'Krzysztof Laskowski',
