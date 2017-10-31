1fa86d798dcb2714897dc09273591c623a6f4d66
kamichal <michal.kaczmarczyk@nokia.com>
2017-10-31 13:07:55 +0100

    refactor of test_supplementation

--- prophyc/tests/test_suplementation.py
+++ prophyc/tests/test_suplementation.py
@@ -1,9 +1,16 @@
 import pytest
-import os
 
 import prophyc
+from collections import namedtuple
+from contextlib import contextmanager
 
-XML_1_CONTENT = """\
+IsarTestItem = namedtuple("IsarTestItem", "file_name_base, input_xml, expected_py")
+
+ISAR_TEST_SET_1 = [
+
+    IsarTestItem(
+        file_name_base="isar_root_defs",
+        input_xml="""\
 <x>
     <typedef name="IsarDefA" type="r32"/>
     <typedef name="IsarDefB" type="r64"/>
@@ -22,71 +29,8 @@ XML_1_CONTENT = """\
         </member>
     </struct>
 </x>
-"""
-
-XML_2_CONTENT = """\
-<x>
-    <include href="isar_root_defs.xml"/>
-    <struct name="IsarK">
-        <member name="ifK_a" type="u8"/>
-        <member name="ifK_B" type="IsarDefB">
-            <dimension size="IsarCONST_A"/>
-        </member>
-    </struct>
-    <struct name="IsarL">
-        <member name="theBItems" type="IsarDefB">
-            <dimension isVariableSize="true" variableSizeFieldName="numOfItems"/>
-        </member>
-    </struct>
-</x>
-"""
-
-XML_3_CONTENT = """\
-<x>
-    <include href="isar_root_defs.xml"/>
-    <struct name="IsarV">
-        <member name="ifV_a" type="IsarDefA"/>
-        <member name="ifV_b" type="IsarDefB">
-            <dimension isVariableSize="true" size="IsarCONST_B"/>
-        </member>
-        <member name="ifV_c" type="EIsarDefEnum"/>
-    </struct>
-</x>
-"""
-
-@pytest.clang_installed
-def test_sack_includes_isar(tmpdir_cwd, tmpfiles_cwd):
-
-    input_file_names = ("isar_root_defs.xml",
-                        "included_by_sack_a.xml",
-                        "included_by_sack_b.xml",
-                        "the_sack.cpp")
-
-    xml1, xml2, xml3, cpp = tmpfiles_cwd(*input_file_names)
-
-    output_file_names = map(lambda f: os.path.splitext(f)[0] + '.py', input_file_names)
-    xml_py, xml2_py, xml3_py, cpp_py = output_files = tmpfiles_cwd(*output_file_names)
-
-    xml1.write(XML_1_CONTENT)
-    xml2.write(XML_2_CONTENT)
-    xml3.write(XML_3_CONTENT)
-
-    cpp.write("""\
-#include <stdint.h>
-struct cppX
-{
-    IsarK defined_in_xml;
-    IsarDefC defined_deeper_in_xmls;
-    uint16_t regular_type;
-    IsarDefA typedefed_deeper_in_xmls;
-};
-""")
-    prophyc.main(["--sack", "--include_isar", str(xml2), "--include_isar", str(xml3),
-                  "--python_out", str(tmpdir_cwd), str(cpp)])
-
-    assert all(map(os.path.isfile, map(str, output_files))), "Some files not created"
-
-    assert xml_py.read() == """\
+""",
+        expected_py="""\
 import prophy
 
 IsarCONST_A = 4
@@ -110,9 +54,28 @@ class IsarDefC(prophy.with_metaclass(prophy.struct_generator, prophy.struct)):
     _descriptor = [('ifC_a', prophy.u16),
                    ('ifC_b_len', prophy.u32),
                    ('ifC_b', prophy.array(prophy.u64, bound = 'ifC_b_len'))]
-"""
+"""),
 
-    assert xml2_py.read() == """\
+
+    IsarTestItem(
+        file_name_base="included_by_sack_a",
+        input_xml="""\
+<x>
+    <include href="isar_root_defs.xml"/>
+    <struct name="IsarK">
+        <member name="ifK_a" type="u8"/>
+        <member name="ifK_B" type="IsarDefB">
+            <dimension size="IsarCONST_A"/>
+        </member>
+    </struct>
+    <struct name="IsarL">
+        <member name="theBItems" type="IsarDefB">
+            <dimension isVariableSize="true" variableSizeFieldName="numOfItems"/>
+        </member>
+    </struct>
+</x>
+""",
+        expected_py="""\
 import prophy
 
 from isar_root_defs import *
@@ -124,9 +87,23 @@ class IsarK(prophy.with_metaclass(prophy.struct_generator, prophy.struct)):
 class IsarL(prophy.with_metaclass(prophy.struct_generator, prophy.struct)):
     _descriptor = [('numOfItems', prophy.u32),
                    ('theBItems', prophy.array(IsarDefB, bound = 'numOfItems'))]
-"""
+"""),
 
-    assert xml3_py.read() == """\
+    IsarTestItem(
+        file_name_base="included_by_sack_b",
+        input_xml="""\
+<x>
+    <include href="isar_root_defs.xml"/>
+    <struct name="IsarV">
+        <member name="ifV_a" type="IsarDefA"/>
+        <member name="ifV_b" type="IsarDefB">
+            <dimension isVariableSize="true" size="IsarCONST_B"/>
+        </member>
+        <member name="ifV_c" type="EIsarDefEnum"/>
+    </struct>
+</x>
+""",
+        expected_py="""\
 import prophy
 
 from isar_root_defs import *
@@ -136,9 +113,46 @@ class IsarV(prophy.with_metaclass(prophy.struct_generator, prophy.struct)):
                    ('ifV_b_len', prophy.u32),
                    ('ifV_b', prophy.array(IsarDefB, bound = 'ifV_b_len', size = IsarCONST_B)),
                    ('ifV_c', EIsarDefEnum)]
-"""
+""")
+]
 
-    assert cpp_py.read() == """\
+
+@pytest.fixture
+def isar_test_helper(tmpdir_cwd):
+    @contextmanager
+    def check_isars_generated(isar_test_set):
+        names, xml_contents, expected_py_contents = zip(*isar_test_set)
+        xmls, pys = zip(*map(lambda b: (tmpdir_cwd.join(b + '.xml'), tmpdir_cwd.join(b + '.py')), names))
+        list(map(lambda t: t[0].write(t[1]), zip(xmls, xml_contents)))
+
+        yield xmls
+
+        for py_file, py_content in zip(pys, expected_py_contents):
+            assert py_file.read() == py_content
+
+    return check_isars_generated
+
+
+@pytest.clang_installed
+def test_sack_supples(isar_test_helper, tmpdir_cwd):
+    cpp = tmpdir_cwd.join('the_sack.hpp')
+    cppy = tmpdir_cwd.join('the_sack.py')
+    cpp.write("""\
+#include <stdint.h>
+struct cppX
+{
+    IsarK defined_in_xml;
+    IsarDefC defined_deeper_in_xmls;
+    uint16_t regular_type;
+    IsarDefA typedefed_deeper_in_xmls;
+};
+""")
+
+    with isar_test_helper(ISAR_TEST_SET_1) as isars:
+        prophyc.main(["--sack", "--include_isar", str(isars[1]), "--include_isar", str(isars[2]),
+                      "--python_out", str(tmpdir_cwd), str(cpp)])
+
+    assert cppy.read() == """\
 import prophy
 
 from included_by_sack_a import *
